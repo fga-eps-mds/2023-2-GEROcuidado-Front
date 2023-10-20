229c6923149762f33d95744619d5234f6a50629c
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-10-19 21:49:16 -0300

    Configurando projeto para Typescript tsconfig.json

---
+++ src/components/Title/index.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+import React from "react"
+
+export default function Title() {
+  return (
+    <View>
+      <Text>GEROcuidado</Text>
+    </View>
+  );
+}
+
+
