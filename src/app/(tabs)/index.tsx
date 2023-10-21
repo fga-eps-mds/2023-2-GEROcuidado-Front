ac2d5d794d0cb99ff88ea0c83d2159f86b22a16b
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-10-21 14:51:23 -0300

    Adicionando components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,30 @@
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
+import { Button } from "../../components/Button"
 import { Link } from "expo-router";
 
-export default function Home(){
+export default function Home() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
             <Text style={{ fontSize: 44, fontWeight: "700"}}>
                 Home - GEROcuidado
             </Text>
 
-            <Link href ="/settings" asChild>
-                <Button title="Configurações" />
+            <Link href="/settings" asChild>
+                <View style={{ marginTop: 20 }}>
+                    <Button title="Configurações" />
+                </View>
             </Link>
+
+            <Link href="/pages/5" asChild>
+                <View style={{ marginTop: 20 }}>
+                    <Button title="Comentário" />
+                </View>    
+            </Link>
+
+            <View style={{ marginTop: 20 }}>
+                <Button title="Ver comentário" />
+            </View>
         </View>
     )
 }
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,7 +1,6 @@
 import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
-import React from 'react';
 
 export default function Settings() {
   return (
