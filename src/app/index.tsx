6a2a575db05f138172702eedfccd86023db29c14
Henrique Melo <hacmelo@gmail.com>
2023-10-21 16:38:06 -0300

    (#130) adiciona tela de inicio

---
+++ src/app/index.tsx
@@ -0,0 +1,32 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { LinkButton } from "../components/LinkButton";
+
+
+export default function Home() {
+  return (
+    <View style={styles.center}>
+      <Image source={require('../../assets/logo.png')} />
+      <Text style={styles.titulo}>
+        Seja um GEROcuidador!
+      </Text>
+      <LinkButton title="Acessar Fórum" href="/" />
+      <LinkButton title="Login" href="/pages/login" />
+      <LinkButton title="Cadastre-se" backgroundColor="#B47B9D" href="/pages/cadastro" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  titulo: {
+    fontSize: 37,
+    fontWeight: "700",
+    textAlign: "center",
+    margin: 20,
+    marginBottom: 70
+  }
+})
---
+++ src/app/pages/login.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Login(){
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text style={{ fontSize: 44, fontWeight: "700"}}>
+                Login
+            </Text>
+        </View>
+    )
+}
