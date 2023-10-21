75689baf9099f7d67da6b1502674aa4fa8df6b1e
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-10-21 19:22:04 -0300

    Adicionando página do Fórum

---
+++ src/app/pages/Forum/index.tsx
@@ -0,0 +1,12 @@
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function Forum() {
+  return (
+
+      <View>
+          <Text>Teste - Forum</Text>
+      </View>
+  );
+}
