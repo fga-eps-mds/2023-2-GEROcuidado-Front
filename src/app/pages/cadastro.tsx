c24993679cde77c7e84747da3846ee76d6d12baa
Pedro Cella <pedrovscella@gmail.com>
2023-10-21 23:12:04 -0300

    Iniciando tela de cadastro

--- src/app/pages/cadastro.tsx
+++ src/app/pages/cadastro.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
-import { Link } from 'expo-router';
+import React, { useState } from "react";
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-export default function Cadastro(){
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
-            <Text style={{ fontSize: 44, fontWeight: "700"}}>
-                Cadastro
-            </Text>
-            <Link href="/" asChild>
-                <Text>voltar home</Text>
-            </Link>
-        </View>
-    )
+export default function Cadastro() {
+  const [nome, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmaEmail, setConfirmaEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
+  return (
+    <View style={styles.container}>
+      <TextInput
+        onChangeText={setName}
+        value={nome}
+        placeholder="Nome completo"
+      />
+      <TextInput onChangeText={setEmail} value={email} placeholder="Email" />
+      <TextInput
+        onChangeText={setConfirmaEmail}
+        value={confirmaEmail}
+        placeholder="Confirme seu Email"
+      />
+      <TextInput onChangeText={setSenha} value={senha} placeholder="Senha" />
+      <TextInput
+        onChangeText={setConfirmaSenha}
+        value={confirmaSenha}
+        placeholder="Confirme sua senha"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text>Cadastrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  field: {},
+  button: {
+    width: "80%",
+    maxWidth: 350,
+    paddingVertical: 16,
+    paddingHorizontal: 26,
+    alignItems: "center",
+    borderRadius: 20,
+    backgroundColor: "#2CCDB5",
+  },
+});
