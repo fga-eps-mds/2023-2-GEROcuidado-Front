8e88c6dc2d99c30e34f6b175a6288c8ce05c4a14
Gustavo <gugustavo137@gmail.com>
2023-10-22 20:53:35 -0300

    Adicionando mock para simular os posts
    Co-authored-by: martinsglucas <martin5.luca5.univ@gmail.com>

--- src/app/pages/(Forum)/index.tsx
+++ src/app/pages/(Forum)/index.tsx
@@ -1,6 +1,20 @@
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+type Post = {
+  id: number;
+  title: string;
+  content: string;
+  likes: number;
+  comments: number;
+};
+
+const posts: Post[] = [
+  { id: 1, title: 'Post 1', content: 'Content of Post 1', likes: 10, comments: 5 },
+  { id: 2, title: 'Post 2', content: 'Content of Post 2', likes: 7, comments: 3 },
+];
 
 export default function Forum() {
   return (
