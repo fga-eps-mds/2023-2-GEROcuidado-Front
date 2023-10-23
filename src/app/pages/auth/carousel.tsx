e34691e2fabb690759cc0972fa469256d1c6f833
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-10-23 17:35:38 -0300

    (#152) Atualiza Carousel

--- src/app/pages/auth/carousel.tsx
+++ src/app/pages/auth/carousel.tsx
@@ -5,6 +5,9 @@ import { LinkButton } from "../../../components/LinkButton";
 import Swiper from "react-native-swiper";
 import { Link } from "expo-router";
 import { NextButton } from "../../../components/NextButton";
+import Tutorial1 from "./tutorial1";
+import Tutorial2 from "./tutorial2";
+import Tutorial3 from "./tutorial3";
 
 export default function Carousel() {
 
@@ -22,125 +25,35 @@ export default function Carousel() {
     }; */
 
     return (
-        <View>
-            <Swiper showsButtons={true}>
+        <View style={styles.carousel} >
+            <Swiper  dotColor="#D9D9D9" activeDotColor="#9E9696" showsButtons={true} loop={false}>
                 <View>
-                    <View>
-                        <Link href="/" asChild>
-                            <TouchableOpacity >
-                                <Text style={styles.skip}>Pular</Text>
-                            </TouchableOpacity>
-                        </Link>
-                    </View>
-                    <View>
-                        <Image
-                            style={styles.image}
-                            source={require("../../../../assets/img_tutor1.png")}
-                        />
-                        <Text style={styles.text}>
-                            Gerencie as <Text style={styles.word}>rotinas</Text>
-                        </Text>
-                        <Text style={styles.text}>do seu idoso</Text>
-                        <View style={styles.linkButton}>
-                            <LinkButton
-                                title="Prosseguir"
-                                href="/pages/auth/carousel"
-                            ></LinkButton>
-                        </View>
-                    </View>
+                     <Tutorial1></Tutorial1>
                 </View>
                 <View>
-                    <View>
-                        <Link href="/" asChild>
-                            <TouchableOpacity>
-                                <Text style={styles.skip}>Pular</Text>
-                            </TouchableOpacity>
-                        </Link>
-                    </View>
-                    <View>
-                        <Image
-                            style={styles.image}
-                            source={require("../../../../assets/img_tutor2.png")}
-                        />
-                        <Text style={styles.text}>Colete os dados da</Text>
-                        <Text style={styles.text}>
-                            <Text style={styles.word}>saúde</Text> do idoso
-                        </Text>
-                        <View style={styles.linkButton}>
-                            <LinkButton
-                                title="Prosseguir"
-                                href="/pages/auth/tutorial3"
-                            ></LinkButton>
-                        </View>
-                    </View>
+                    <Tutorial2></Tutorial2>
                 </View>
                 <View>
-                    <View>
-                        <Link href="/" asChild>
-                            <TouchableOpacity>
-                                <Text style={styles.skip}>Pular</Text>
-                            </TouchableOpacity>
-                        </Link>
-                    </View>
-                    <View>
-                        <Image
-                            style={styles.image}
-                            source={require("../../../../assets/img_tutor3.png")}
-                        />
-                        <Text style={styles.text}>Obtenha ajuda no</Text>
-                        <Text style={[styles.text, styles.word]}>portal</Text>
-                        <View style={styles.linkButton}>
-                            <LinkButton
-                                title="Prosseguir"
-                                href="/pages/auth/tutorial1"
-                            ></LinkButton>
-                        </View>
-                    </View>
+                    <Tutorial3></Tutorial3>
                 </View>
             </Swiper>
+            <View style={styles.linkButton}>
+                <LinkButton
+                    title="Prosseguir"
+                    href="/"
+                ></LinkButton>
+             </View>
        </View>
     );
 }
 
 
 const styles = StyleSheet.create({
-    skip: {
-        textAlign: "right",
-        marginTop: 10,
-        marginBottom: 10,
-        marginRight: 10,
-        fontWeight: "bold",
-        color: "gray",
-    },
-    image: {
-        alignSelf: "center",
-        marginBottom: 28,
-        marginTop: 32,
-    },
-    text: {
-        fontSize: 32,
-        textAlign: "center",
-        fontWeight: "bold",
-        color: "gray",
-    },
-    word: {
-        color: "#B4026D",
-    },
-    row: {
-        marginTop: 40,
-        flexDirection: "row",
-        alignSelf: "center",
-    },
-    circle: {
-        color: "#9E9696",
-        paddingLeft: 10,
-        paddingRight: 10,
-    },
-    notSelectedCircle: {
-        opacity: 0.4,
-    },
     linkButton: {
         alignItems: "center",
         marginTop: 27,
+    },
+    carousel:{
+        height:"100%",
     },
 });
