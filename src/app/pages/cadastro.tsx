054cf5356117efc65e894fbc3906e6072abeadb1
Henrique Melo <hacmelo@gmail.com>
2023-10-22 21:35:38 -0300

    (#131) adiciona scroll view

--- src/app/pages/cadastro.tsx
+++ src/app/pages/cadastro.tsx
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Platform,
   Button,
+  ScrollView,
 } from "react-native";
 import { Link } from "expo-router";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -23,80 +24,82 @@ export default function Cadastro() {
   const [escondeConfirmaSenha, setEscondeConfirmaSenha] = useState(true);
 
   return (
-    <View >
-
+    <View>
       <Link href="/" asChild>
         <TouchableOpacity >
           <Icon name="chevron-left" size={42} />
         </TouchableOpacity>
       </Link>
 
-      <UploadImage />
+      <ScrollView>
 
-      <View style={styles.field}>
-        <Icon style={styles.iconInput} name="account-outline" size={20} />
-        <TextInput
-          onChangeText={setNome}
-          value={nome}
-          placeholder="Nome completo"
-          style={styles.textInput}
-        />
-      </View>
+        <UploadImage />
 
-      <View style={styles.field}>
-        <Icon style={styles.iconInput} name="email-outline" size={20} />
-        <TextInput
-          onChangeText={setEmail}
-          value={email}
-          placeholder="Email"
-          style={styles.textInput}
-        />
-      </View>
+        <View style={styles.field}>
+          <Icon style={styles.iconInput} name="account-outline" size={20} />
+          <TextInput
+            onChangeText={setNome}
+            value={nome}
+            placeholder="Nome completo"
+            style={styles.textInput}
+          />
+        </View>
 
-      <View style={styles.field}>
-        <Icon style={styles.iconInput} name="email-outline" size={20} />
-        <TextInput
-          onChangeText={setConfirmaEmail}
-          value={confirmaEmail}
-          placeholder="Confirme seu Email"
-          style={styles.textInput}
-        />
-      </View>
+        <View style={styles.field}>
+          <Icon style={styles.iconInput} name="email-outline" size={20} />
+          <TextInput
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Email"
+            style={styles.textInput}
+          />
+        </View>
 
-      <View style={styles.field}>
-        <Icon style={styles.iconInput} name="lock-outline" size={20} />
-        <TextInput
-          onChangeText={setSenha}
-          value={senha}
-          placeholder="Senha"
-          secureTextEntry={escondeSenha}
-          style={styles.passwordInput}
-        />
+        <View style={styles.field}>
+          <Icon style={styles.iconInput} name="email-outline" size={20} />
+          <TextInput
+            onChangeText={setConfirmaEmail}
+            value={confirmaEmail}
+            placeholder="Confirme seu Email"
+            style={styles.textInput}
+          />
+        </View>
 
-        <Icon onPress={() => setEscondeSenha(!escondeSenha)} style={styles.passwordIcon} name={escondeSenha ? "eye-outline" : "eye-off-outline"} size={20} />
-      </View>
+        <View style={styles.field}>
+          <Icon style={styles.iconInput} name="lock-outline" size={20} />
+          <TextInput
+            onChangeText={setSenha}
+            value={senha}
+            placeholder="Senha"
+            secureTextEntry={escondeSenha}
+            style={styles.passwordInput}
+          />
 
-      <View style={styles.field}>
-        <Icon style={styles.iconInput} name="lock-outline" size={20} />
-        <TextInput
-          onChangeText={setConfirmaSenha}
-          value={confirmaSenha}
-          placeholder="Confirme sua senha"
-          secureTextEntry={escondeConfirmaSenha}
-          style={styles.passwordInput}
-        />
-        <Icon onPress={() => setEscondeConfirmaSenha(!escondeConfirmaSenha)} style={styles.passwordIcon} name={escondeConfirmaSenha ? "eye-outline" : "eye-off-outline"} size={20} />
-      </View>
+          <Icon onPress={() => setEscondeSenha(!escondeSenha)} style={styles.passwordIcon} name={escondeSenha ? "eye-outline" : "eye-off-outline"} size={20} />
+        </View>
 
-      <View style={styles.linkButton}>
-        <LinkButton title="Cadastrar" href="/pages/cadastro" />
-      </View>
+        <View style={styles.field}>
+          <Icon style={styles.iconInput} name="lock-outline" size={20} />
+          <TextInput
+            onChangeText={setConfirmaSenha}
+            value={confirmaSenha}
+            placeholder="Confirme sua senha"
+            secureTextEntry={escondeConfirmaSenha}
+            style={styles.passwordInput}
+          />
+          <Icon onPress={() => setEscondeConfirmaSenha(!escondeConfirmaSenha)} style={styles.passwordIcon} name={escondeConfirmaSenha ? "eye-outline" : "eye-off-outline"} size={20} />
+        </View>
+
+        <View style={styles.linkButton}>
+          <LinkButton title="Cadastrar" href="/pages/cadastro" />
+        </View>
+      </ScrollView>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
   button: {
     width: "80%",
     maxWidth: 350,
@@ -125,6 +128,7 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     color: "#05375a",
     width: "80%",
+    fontSize: 17,
   },
   passwordIcon: {
     width: "10%",
@@ -133,12 +137,14 @@ const styles = StyleSheet.create({
     width: "90%",
     paddingLeft: 10,
     color: "#05375a",
+    fontSize: 17,
   },
   arrow: {
     alignSelf: "flex-start",
   },
   linkButton: {
     marginTop: 90,
+    marginBottom: 60,
     alignItems: "center",
   },
 });
