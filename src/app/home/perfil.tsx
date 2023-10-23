642bc9d9b84ed495c3b98538c7f6d3bcab255d75
Gustavo <gugustavo137@gmail.com>
2023-10-22 23:08:29 -0300

    Adicionando barra de busca na pagina do forum
    Co-authored-by: martinsglucas <martin5.luca5.univ@gmail.com>

--- src/app/home/perfil.tsx
+++ src/app/home/perfil.tsx
@@ -13,18 +13,30 @@ type Post = {
 const posts: Post[] = [
   { id: 1, title: 'Post 1', content: 'Content of Post 1', likes: 10, comments: 5 },
   { id: 2, title: 'Post 2', content: 'Content of Post 2', likes: 7, comments: 3 },
+  { id: 3, title: 'Post 3', content: 'Content of Post 3', likes: 17, comments: 2 },
+  { id: 4, title: 'Post 4', content: 'Content of Post 4', likes: 14, comments: 6 },
+  { id: 5, title: 'Post 5', content: 'Content of Post 5', likes: 2, comments: 7 },
+  { id: 6, title: 'Post 6', content: 'Content of Post 6', likes: 5, comments: 8 },
+  { id: 7, title: 'Post 7', content: 'Content of Post 7', likes: 2, comments: 13 },
+  { id: 8, title: 'Post 8', content: 'Content of Post 8', likes: 7, comments: 0 },
+  { id: 9, title: 'Post 9', content: 'Content of Post 9', likes: 9, comments: 1 },
+  { id: 10, title: 'Post 10', content: 'Content of Post 10', likes: 11, comments: 1 },
 ];
 
 export default function Forum() {
   return (
 
       <View>
-          <Text>Teste - Forum</Text>
+        {/* Fixed Search Bar */}
+        <View style={{ backgroundColor: 'lightgray', padding: 10 }}>
 
-          {/* Fixed Search Bar */}
-      <View style={{ backgroundColor: 'lightgray', padding: 10 }}>
-        {/* Your search bar component */}
-        <Text>Search Bar</Text>
+        <Text>Fórum Gero Cuidado</Text>
+        
+        <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10, padding: 5 }}
+        placeholder="Search..."
+        // onChangeText={(text) => setSearchText(text)}
+      />
       </View>
 
       {/* Scrollable Posts */}
