1a5be47278ea97fb17462a4a1372dd9f47f401cd
W4RT1N5 <martin5.luca5.univ@gmail.com>
2023-10-23 23:28:21 -0300

    Transferindo barra de pesquisa para página fórum
    Co-authored-by: Thefast777 <111928835+thefast777@users.noreply.github.com>
    Co-authored-by: Eduardo Gurgel <eduardo-gurgel@hotmail.com>
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>

--- src/app/home/forum.tsx
+++ src/app/home/forum.tsx
@@ -1,11 +1,40 @@
-import { FlatList, Image, Text, View } from 'react-native';
-
+import { FlatList, Image, Text, View, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import React from 'react';
 
 export default function HomeScreen() {
   return (
+
     <View style={{ flex: 1, backgroundColor: "white" }}>
-      
+      <View>
+        {/* Fixed Search Bar */}
+        <View style={{ backgroundColor: "#2CCDB5", padding: 10 }}>
+          <View style={{alignItems:'center'}}>
+            <Icon name='chevron-left' color={'white'} size={60} style={{position:'absolute', left:-20}}></Icon>
+            <Text style={styles.titulo}>Fórum Gero Cuidado</Text>
+          </View>
+
+          <View style={{flexDirection: 'row', alignItems: 'center',}}>
+            <TextInput
+              style={styles.barraDePesquisa}
+              placeholder="Pesquise uma notícia"
+              // onChangeText={(text) => setSearchText(text)}
+            />
+            <TouchableOpacity style={styles.botao} onPress={() => {}}>
+              <Icon style={{position:'absolute', right:15}} name='magnify' color={'#ADADAD'} size={30}></Icon>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ backgroundColor: 'white', padding: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+        <Text style={{fontFamily:'Work Sans', fontSize:24, fontWeight:'600'}}>Publicacoes</Text>
+        <TouchableOpacity style={styles.botao} onPress={() => {}}>
+          <Text style={styles.textoBotao}>Crie uma publicação </Text>
+          <Icon name='pencil' color={'white'} size={25}></Icon>
+        </TouchableOpacity>
+      </View>
+
       <FlatList
         data={["1", "2", "3"]}
         renderItem={({ item }) => (
@@ -33,3 +62,37 @@ export default function HomeScreen() {
     </View>
   )
 }
+const styles = StyleSheet.create({
+  titulo:{
+    fontFamily:'Inter', 
+    fontWeight:'bold', 
+    color:'white', 
+    textAlign:'center',
+    fontSize: 24,
+    padding:20,
+  },
+  barraDePesquisa:{
+    flex:1, height: 40, 
+    borderColor: 'white', 
+    textAlign:'center', 
+    color:'#ADADAD', 
+    backgroundColor:'white', 
+    borderWidth: 1, 
+    margin: 10, 
+    padding: 5, 
+    borderRadius:14
+  },
+  botao: {
+    flexDirection:'row',
+    alignItems:'center',
+    backgroundColor: '#B4026D',
+    padding: 1,
+    borderRadius: 14,
+  },
+  textoBotao: {
+    color:'white',
+    fontFamily:'Work Sans',
+    fontWeight:'600',
+    fontSize:16,
+  },
+})
