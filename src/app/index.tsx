54de7460304763ea82f7f4f0d8e86bc8d501e3d7
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-10-26 13:26:01 -0300

    Conflitos

---
+++ src/app/index.tsx
@@ -0,0 +1,34 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import { LinkButton } from "../components/LinkButton";
+
+export default function Home() {
+  return (
+    <View style={styles.center}>
+      <Image source={require("../../assets/logo.png")} />
+      <Text style={styles.titulo}> Seja um GEROcuidador! </Text>
+      <LinkButton title="Acessar Fórum" href="/" />
+      <LinkButton title="Login" href="/public/login" />
+      <LinkButton
+        title="Cadastre-se"
+        backgroundColor="#B47B9D"
+        href="/public/cadastro"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titulo: {
+    fontSize: 37,
+    fontWeight: "700",
+    textAlign: "center",
+    margin: 20,
+    marginBottom: 70,
+  },
+});
