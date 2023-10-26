e032e050f522ae2d70af08f47ae2dc617f6aa106
Henrique Melo <hacmelo@gmail.com>
2023-10-26 11:00:22 -0300

    (#132) refatora user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,44 +1,44 @@
 import { IResponse } from "../interfaces/response.interface";
 import { IUser, IUserBody, IUserLogin } from "../interfaces/user.interface";
 
-const baseUrl = 'http://ec2-3-80-226-110.compute-1.amazonaws.com:3001/api/usuario'
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+const API_PORT = process.env.EXPO_PUBLIC_API_USUARIO_PORT;
+const BASE_URL = `${API_URL}:${API_PORT}/api/usuario`
 
-export default class UserService {
-  async postUser(body: IUserBody): Promise<IResponse<IUser | null>>{
-    const response = await fetch(baseUrl, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
+export const postUser = async (body: IUserBody): Promise<IResponse<IUser | null>> => {
+  const response = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
 
-    const json = await response.json();
+  const json = await response.json();
 
-    if (response.status !== 201) {
-      throw new Error(json.message as string)
-    }
-
-    return json;
+  if (response.status !== 201) {
+    throw new Error(json.message as string)
   }
 
-  async loginUser(body: IUserLogin): Promise<IResponse<string>>{
-    const response = await fetch(`${baseUrl}/login`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
+  return json;
+}
 
-    const json = await response.json();
+export const loginUser = async (body: IUserLogin): Promise<IResponse<string>> => {
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
 
-    if (response.status !== 200) {
-      throw new Error(json.message as string)
-    }
+  const json = await response.json();
 
-    return json;
+  if (response.status !== 200) {
+    throw new Error(json.message as string)
   }
+
+  return json;
 }
