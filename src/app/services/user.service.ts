a3fb125620553cbda54d838a9a5b949532286c7d
Henrique Melo <hacmelo@gmail.com>
2023-10-26 01:00:26 -0300

    (#132) Adiciona servico de usuario
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { IResponse } from "../interfaces/response.interface";
-import { IUser, IUserBody } from "../interfaces/user.interface";
+import { IUser, IUserBody, IUserLogin } from "../interfaces/user.interface";
 
 const baseUrl = 'http://localhost:3001/api/usuario'
 
@@ -17,6 +17,25 @@ export default class UserService {
     const json = await response.json();
 
     if (response.status !== 201) {
+      throw new Error(json.message as string)
+    }
+
+    return json;
+  }
+
+  async loginUser(body: IUserLogin): Promise<IResponse<string>>{
+    const response = await fetch(`${baseUrl}/login`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+
+    const json = await response.json();
+
+    if (response.status !== 200) {
       throw new Error(json.message as string)
     }
 
