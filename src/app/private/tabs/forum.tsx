7e2a6c4e4ac8b8b13a8c7183d2f65be9f6cd14de
martinsglucas <martin5.luca5.univ@gmail.com>
2023-10-28 15:14:35 -0300

    (#156) organiza tela fórum

--- src/app/private/tabs/forum.tsx
+++ src/app/private/tabs/forum.tsx
@@ -1,9 +1,10 @@
-import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useState } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import BackButton from '../../components/BackButton';
 import LinkButton from '../../components/LinkButton';
+import { Link, router } from 'expo-router';
 
 // CRIANDO OBJETOS DE COMENTÁRIOS
 export default function HomeScreen() {
@@ -50,80 +51,10 @@ export default function HomeScreen() {
   const handleComment = (postId: string) => {
     // Quando clicar no comentário o que acontece???
   };
-
-  const styles = StyleSheet.create({
-    postContainer: {
-      margin: 10,
-      borderRadius: 10,
-      elevation: 5,
-      backgroundColor: 'white',
-      shadowColor: '#333',
-      shadowOffset: { width: 0, height: 1 },
-      shadowOpacity: 0.5,
-      shadowRadius: 2,
-    },
-    titulo:{
-
-      fontWeight:'bold', 
-      color:'white',
-      fontSize: 24,
-      padding:20,
-    },
-    barraDePesquisa:{
-      flex:1, height: 40, 
-      borderColor: 'white', 
-      textAlign:'center', 
-      color:'#ADADAD', 
-      backgroundColor:'white', 
-      borderWidth: 1, 
-      margin: 10, 
-      padding: 5, 
-      borderRadius:14
-    },
-    botao: {
-      flexDirection:'row',
-      alignItems:'center',
-      backgroundColor: '#B4026D',
-      padding: 1,
-      borderRadius: 14,
-    },
-    textoBotao: {
-      color:'white',
-
-      fontWeight:'600',
-      fontSize:16,
-    },
-    avatar: {
-      height: 50,
-      width: 50,
-      borderRadius: 50,
-    },
-    postHeader: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 15,
-    },
-    userInfo: {
-      marginLeft: 10,
-    },
-    username: {
-      fontSize: 20,
-    },
-    date: {
-      fontSize: 12,
-    },
-    postContent: {
-      fontSize: 15,
-      maxHeight: 100,
-      padding: 6,
-      textAlign: 'justify',
-    },
-    actions: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      padding: 10,
-    },
-  });
+  
+  const novaPublicacao = () => {
+    router.push({ pathname: "/private/tabs/perfil"});
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: 'white' }}>
@@ -132,7 +63,9 @@ export default function HomeScreen() {
         <View style={{ backgroundColor: "#2CCDB5", padding: 10 }}>
           <View style={{flexDirection:"row", alignItems:"center"}}>
             <View>
-              <BackButton/>
+              <Link href="">
+                <Icon name="chevron-left" size={60} style={{color:"white", alignSelf: "flex-start" }} />
+              </Link>
             </View>
             <View>
               <Text style={styles.titulo}>Fórum Gero Cuidado</Text>
@@ -145,18 +78,18 @@ export default function HomeScreen() {
               placeholder="Pesquise uma notícia"
               // onChangeText={(text) => setSearchText(text)}
             />
-            <TouchableOpacity style={styles.botao} onPress={() => {}}>
+            <Pressable style={styles.botao} onPress={() => {}}>
               <Icon style={{position:'absolute', right:15}} name='magnify' color={'#ADADAD'} size={30}></Icon>
-            </TouchableOpacity>
+            </Pressable>
           </View>
 
         </View>
         <View style={{ backgroundColor: 'white', padding: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
             <Text style={{fontSize:24, fontWeight:'600'}}>Publicações</Text>
-            <TouchableOpacity style={styles.botao} onPress={() => {}}>
+            <Pressable style={styles.botao} onPress={novaPublicacao}>
               <Text style={styles.textoBotao}>Crie uma publicação </Text>
               <Icon name='pencil' color={'white'} size={25}></Icon>
-            </TouchableOpacity>
+            </Pressable>
           </View>   
       </View>
       <FlatList
@@ -191,3 +124,77 @@ export default function HomeScreen() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  postContainer: {
+    margin: 10,
+    borderRadius: 10,
+    elevation: 5,
+    backgroundColor: 'white',
+    shadowColor: '#333',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+  },
+  titulo:{
+
+    fontWeight:'bold', 
+    color:'white',
+    fontSize: 24,
+    padding:20,
+  },
+  barraDePesquisa:{
+    flex:1, height: 40, 
+    borderColor: 'white', 
+    textAlign:'center', 
+    color:'#ADADAD', 
+    backgroundColor:'white', 
+    borderWidth: 1, 
+    margin: 10, 
+    padding: 5, 
+    borderRadius:14
+  },
+  botao: {
+    flexDirection:'row',
+    alignItems:'center',
+    backgroundColor: '#B4026D',
+    padding: 1,
+    borderRadius: 14,
+  },
+  textoBotao: {
+    color:'white',
+
+    fontWeight:'600',
+    fontSize:16,
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  postHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+  },
+  userInfo: {
+    marginLeft: 10,
+  },
+  username: {
+    fontSize: 20,
+  },
+  date: {
+    fontSize: 12,
+  },
+  postContent: {
+    fontSize: 15,
+    maxHeight: 100,
+    padding: 6,
+    textAlign: 'justify',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+});
