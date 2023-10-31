8fcfbd1f97fc137bf77c54ba1c2addb353d21939
Pedro Cella <pedrovscella@gmail.com>
2023-10-31 18:36:57 -0300

    Adiciona novo teste no CustomButton

--- src/app/__tests__/CustomButton.spec.tsx
+++ src/app/__tests__/CustomButton.spec.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from "@testing-library/react-native";
 import CustomButton from "../components/CustomButton";
 
-describe('CustomButton', () => {
-    it('renderiza corretamente', () => {
-        const { toJSON } = render(<CustomButton title="teste" callbackFn={jest.fn()}/>);
-        expect(toJSON()).toBeTruthy()
-    })
-})
+describe("CustomButton", () => {
+  it("renderiza corretamente", () => {
+    const { toJSON } = render(
+      <CustomButton title="teste" callbackFn={jest.fn()} />,
+    );
+    expect(toJSON()).toBeTruthy();
+  });
+  it("Chama a função de callback quando pressionado", () => {
+    const callbackFn = jest.fn();
+    const { getByText } = render(
+      <CustomButton title="Test" callbackFn={callbackFn} />,
+    );
+    const button = getByText("Test");
+    fireEvent.press(button);
+    expect(callbackFn).toHaveBeenCalled();
+  });
+});
