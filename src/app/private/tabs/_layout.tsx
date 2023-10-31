805f764d17808b2ba402863287f25774258441e2
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-10-31 01:59:45 -0300

    Adicionando Botão Central

--- src/app/private/tabs/_layout.tsx
+++ src/app/private/tabs/_layout.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import Toast from "react-native-toast-message";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { Platform, StyleSheet, View } from "react-native";
+
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import React from 'react';
 import { Tabs } from "expo-router";
+import Toast from "react-native-toast-message";
 
 export default function TabsLayout() {
   const iconComponent = (focused: boolean, size: number, iconName: string) => (
@@ -23,7 +24,7 @@ export default function TabsLayout() {
           tabBarActiveTintColor: "lightgrey",
           tabBarStyle: {
             backgroundColor: "#2CCDB5",
-            height: 70,
+            height: 65,
           },
           tabBarLabelStyle: {
             marginBottom: Platform.OS === "ios" ? -15 : 10,
@@ -51,6 +52,28 @@ export default function TabsLayout() {
             tabBarIcon: ({ size, focused }) => {
               return iconComponent(focused, size, "heart");
             },
+          }}
+        />
+        <Tabs.Screen
+          name="central"
+          options={{
+            title: "",
+            tabBarIcon: ({ focused }: { focused : boolean}) => {
+              return(
+                <View style={{                  
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: "#B4026D",
+                  width: 70,
+                  height: 70,
+                  top: -7,
+                  borderRadius: 35,
+
+                }}>
+                  <Icon name="plus" size={50} color="white" />
+                </View>
+              )
+            }
           }}
         />
         <Tabs.Screen
@@ -84,4 +107,15 @@ const styles = StyleSheet.create({
     marginTop: Platform.OS === "ios" ? 10 : 0,
     marginBottom: Platform.OS === "ios" ? 0 : -10,
   },
+
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  }
 });
