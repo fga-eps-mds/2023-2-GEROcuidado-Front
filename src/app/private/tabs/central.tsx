918b87c0b10d253746a0729b2361573c9deb4310
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-10-31 01:59:38 -0300

    Adicionando Página do Botão Central

---
+++ src/app/private/tabs/central.tsx
@@ -0,0 +1,26 @@
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+import React from 'react';
+
+const BotaoCentral= ({}) => {
+    return (
+        <View style={styles.container}>
+            <Text>Botão Central</Text>
+            <Button
+                title="Aperte aqui"
+                onPress={() => alert('Botão Central!')}
+            />
+        </View>
+    );
+};
+
+export default BotaoCentral;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#8fcbbc'
+    },
+});
