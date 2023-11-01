20ad429d53050e721bbe22b314d50dedc03944ff
art1505 <arthurtalles@yahoo.com.br>
2023-11-01 00:11:32 -0300

    Adiciona novos testes para o index
    Co-authored-by: pfc15 <pedrofcrux@gmail.com>

--- src/app/__tests__/index.spec.tsx
+++ src/app/__tests__/index.spec.tsx
@@ -7,5 +7,19 @@ describe('Home', () => {
         const { toJSON } = render(<Home />);
         expect(toJSON()).toMatchSnapshot();
     })
+    it("deve conter um título", () => {
+        const { getByText } = render(<Home />);
+        const titleElement = getByText("Seja um GEROcuidador!");
+        expect(titleElement).toBeTruthy();
+      });
     
+      it("deve conter botões de ação", () => {
+        const { getByText } = render(<Home />);
+        const forumButton = getByText("Acessar Fórum");
+        const loginButton = getByText("Login");
+        const cadastroButton = getByText("Cadastre-se");
+        expect(forumButton).toBeTruthy();
+        expect(loginButton).toBeTruthy();
+        expect(cadastroButton).toBeTruthy();
+      });
 })
