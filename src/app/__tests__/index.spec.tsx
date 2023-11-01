3072b9eaba5141adb692a3b5e4d39ed54045abfe
art1505 <arthurtalles@yahoo.com.br>
2023-11-01 00:07:51 -0300

    pequena refatoração no teste de index
    Co-authored-by: pfc15 <pedrofcrux@gmail.com>

--- src/app/__tests__/index.spec.tsx
+++ src/app/__tests__/index.spec.tsx
@@ -5,6 +5,7 @@ import Home from "..";
 describe('Home', () => {
     it('renderiza corretamente', () => {
         const { toJSON } = render(<Home />);
-        expect(toJSON())
+        expect(toJSON()).toMatchSnapshot();
     })
+    
 })
