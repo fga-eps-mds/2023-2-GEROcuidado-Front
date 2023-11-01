07a402b54bdfa030ac634db7740f96d1d4f3adf7
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-11-01 00:23:22 -0300

    (#158) Adiciona pressable

--- src/app/components/Publicacao.tsx
+++ src/app/components/Publicacao.tsx
@@ -1,50 +1,55 @@
 import React, { useState } from "react";
-import { FlatList, View, Image, Text, StyleSheet } from "react-native";
+import { FlatList, View, Image, Text, StyleSheet, Pressable } from "react-native";
 import { IPublicacao } from "../interfaces/forum.interface";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Link } from "expo-router";
 
 interface IProps {
-    item : IPublicacao,
+    item: IPublicacao,
 }
 
-export default function Publicacao({item}: Readonly<IProps>) {
+export default function Publicacao({ item }: Readonly<IProps>) {
     const [posts, setPosts] = useState();
-    
-    
+
+
     const hasFoto = (foto: string | null | undefined) => {
         if (!foto) return false;
-        
+
         const raw = foto.split("data:image/png;base64,")[1];
         return raw.length > 0;
     };
-    
+
     const getFoto = (foto: string | null | undefined) => {
         if (hasFoto(foto)) {
             return (
                 <Image source={{ uri: foto as string }} style={styles.fotoPerfil} />
-                );
-            }
-            
-            return (
-                <View style={[styles.semFoto, styles.fotoPerfil]}>
-            <Icon style={styles.semFotoIcon} name="image-outline" size={15} />
-          </View>
+            );
+        }
+
+        return (
+            <View style={[styles.semFoto, styles.fotoPerfil]}>
+                <Icon style={styles.semFotoIcon} name="image-outline" size={15} />
+            </View>
         );
     };
-    
+
 
     return (
-        <View style={styles.postContainer}>
-            <View style={styles.postHeader}>
-                {getFoto(item.usuario.foto)}
-                <View style={styles.userInfo}>
-                    <Text style={styles.title}>{item.titulo}</Text>
-                    <Text style={styles.username}>{item.usuario.nome}</Text>
-                    <Text style={styles.date}>{item.dataHora.toString()}</Text>
+        <Pressable style={styles.postContainer}>
+            <Link href="/private/pages/verificarPostagem">
+                <View >
+                    <View style={styles.postHeader}>
+                        {getFoto(item.usuario.foto)}
+                        <View style={styles.userInfo}>
+                            <Text style={styles.title}>{item.titulo}</Text>
+                            <Text style={styles.username}>{item.usuario.nome}</Text>
+                            <Text style={styles.date}>{item.dataHora.toString()}</Text>
+                        </View>
+                    </View>
+                    <Text style={styles.postContent}>{item.descricao}</Text>
                 </View>
-            </View> 
-            <Text style={styles.postContent}>{item.descricao}</Text>    
-        </View>
+            </Link>
+        </Pressable>
     );
 }
 
@@ -84,15 +89,15 @@ const styles = StyleSheet.create({
         padding: 6,
         textAlign: "justify",
         marginLeft: 10,
-        marginRight: 10,
+        width: "100%",
     },
     fotoPerfil: {
         width: 60,
         aspectRatio: 1,
         borderRadius: 100,
-      },
-      semFoto: { position: "relative", backgroundColor: "#EFEFF0" },
-      semFotoIcon: {
+    },
+    semFoto: { position: "relative", backgroundColor: "#EFEFF0" },
+    semFotoIcon: {
         position: "absolute",
         right: "38%",
         bottom: "38%",
@@ -100,9 +105,9 @@ const styles = StyleSheet.create({
         margin: "auto",
         alignSelf: "center",
         zIndex: 1,
-      },
-      username: {
+    },
+    username: {
         color: '#000000',
         opacity: 0.5
-      }
+    }
 });
