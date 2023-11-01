243030b8795e9e98354393bb7a41e53f11b5ccbf
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-10-31 23:56:58 -0300

    (#153) Cria componente da publicacao e atualiza forum
    Co-authored-by: Guilherme Silva <221021984@aluno.unb.br>
    Co-authored-by: Lucas Martins <martin5.luca5.univ@gmail.com>
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

--- src/app/private/tabs/forum.tsx
+++ src/app/private/tabs/forum.tsx
@@ -7,113 +7,84 @@ import {
   TextInput,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { router } from "expo-router";
+import Publicacao from "../../components/Publicacao";
+import { getAllPublicacao } from "../../services/forum.service";
+import Toast from "react-native-toast-message";
+import { IPublicacao } from "../../interfaces/forum.interface";
+import { ScrollView } from "react-native-gesture-handler";
 
 // CRIANDO OBJETOS DE COMENTÁRIOS
 export default function HomeScreen() {
-  const [posts, setPosts] = useState([
-    {
-      id: "1",
-      username: "Amélia",
-      date: new Date().toString().substring(0, 16),
-      content:
-        "Eu sou uma cuidadora, Eu sou uma cuidadora, Eu sou uma cuidadora, Eu sou uma cuidadora, Eu sou uma cuidadora, Eu sou uma cuidadora, Eu sou uma cuidadora, Eu sou uma cuidadora",
-      likes: 25,
-      comments: 0,
-    },
-    {
-      id: "2",
-      username: "Joana",
-      date: new Date().toString().substring(0, 16),
-      content:
-        "Eu cuido de idosos, e também cuido de outros idosos, e também cuido.",
-      likes: 12,
-      comments: 0,
-    },
-    {
-      id: "3",
-      username: "Ana",
-      date: new Date().toString().substring(0, 16),
-      content:
-        "Eu cuido de idosos, e também cuido de outros idosos, e também cuido.",
-      likes: 32,
-      comments: 0,
-    },
-  ]);
+  const [publicacoes, setPublicacoes] = useState<IPublicacao[]>([]);
 
   const novaPublicacao = () => {
     router.push("private/pages/criaPublicacao");
   };
 
+  const getPublicacoes = () => {
+    getAllPublicacao().then((response) => {
+      setPublicacoes(response.data as IPublicacao[]);
+    }).catch((err) => {
+      const error = err as { message: string };
+      Toast.show({
+        type: "error",
+        text1: "Erro!",
+        text2: error.message,
+      });
+    });
+  };
+
+  useEffect(() => getPublicacoes(), []);
+
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <View>
-        {/* BARRA DE PESQUISA */}
-        <View style={styles.cabecalho}>
-          <View style={styles.barraDePesquisa}>
-            <TextInput
-              style={styles.inputBarraDePesquisa}
-              placeholder="Pesquise uma notícia"
+    <ScrollView>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <View>
+          {/* BARRA DE PESQUISA */}
+          <View style={styles.cabecalho}>
+            <View style={styles.barraDePesquisa}>
+              <TextInput
+                style={styles.inputBarraDePesquisa}
+                placeholder="Pesquise uma notícia"
               // onChangeText={(text) => setSearchText(text)}
-            />
-            <Pressable style={styles.botaoPesquisar} onPress={() => {}}>
-              <Icon
-                style={styles.iconePesquisar}
-                name="magnify"
-                size={30}
-              ></Icon>
+              />
+              <Pressable style={styles.botaoPesquisar} onPress={() => { }}>
+                <Icon
+                  style={styles.iconePesquisar}
+                  name="magnify"
+                  size={30}
+                ></Icon>
+              </Pressable>
+            </View>
+          </View>
+          <View style={styles.publicacao}>
+            <Text style={styles.textoPublicacoes}>Publicações</Text>
+            <Pressable
+              style={styles.botaoCriarPublicacao}
+              onPress={novaPublicacao}
+            >
+              <Text style={styles.textoBotaoPesquisar}>Nova publicação</Text>
+              <Icon name="pencil" color={"white"} size={25}></Icon>
             </Pressable>
           </View>
-        </View>
-        <View style={styles.publicacao}>
-          <Text style={styles.textoPublicacoes}>Publicações</Text>
-          <Pressable
-            style={styles.botaoCriarPublicacao}
-            onPress={novaPublicacao}
-          >
-            <Text style={styles.textoBotaoPesquisar}>Crie uma publicação </Text>
-            <Icon name="pencil" color={"white"} size={25}></Icon>
-          </Pressable>
+          <FlatList
+            data={publicacoes}
+            renderItem={({ item }) => (
+              <Publicacao item={item} />
+            )}
+          />
         </View>
       </View>
-      <FlatList
-        data={posts}
-        renderItem={({ item }) => (
-          <View style={styles.postContainer}>
-            <View style={styles.postHeader}>
-              <Image
-                style={styles.avatar}
-                source={require("../../../../assets/amelia.png")}
-              />
-              <View style={styles.userInfo}>
-                <Text style={styles.username}>{item.username}</Text>
-                <Text style={styles.date}>{item.date}</Text>
-              </View>
-            </View>
-
-            <Text style={styles.postContent}>{item.content}</Text>
-          </View>
-        )}
-        keyExtractor={(item) => item.id}
-      />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  postContainer: {
-    margin: 10,
-    borderRadius: 10,
-    elevation: 5,
-    backgroundColor: "white",
-    shadowColor: "#333",
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.5,
-    shadowRadius: 2,
-  },
+
   cabecalho: {
     backgroundColor: "#2CCDB5",
     padding: 10,
@@ -181,31 +152,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-  },
-  avatar: {
-    height: 50,
-    width: 50,
-    borderRadius: 50,
-  },
-  postHeader: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 15,
-  },
-  userInfo: {
-    marginLeft: 10,
-  },
-  username: {
-    fontSize: 20,
-  },
-  date: {
-    fontSize: 12,
-  },
-  postContent: {
-    fontSize: 15,
-    maxHeight: 100,
-    padding: 6,
-    textAlign: "justify",
   },
   actions: {
     flexDirection: "row",
