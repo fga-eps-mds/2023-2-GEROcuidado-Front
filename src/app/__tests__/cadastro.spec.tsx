8cc3bb97f415838b73b64eb3f75683991390b4b4
art1505 <arthurtalles@yahoo.com.br>
2023-11-02 23:46:06 -0300

    Complementa testes do cadastro

--- src/app/__tests__/cadastro.spec.tsx
+++ src/app/__tests__/cadastro.spec.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import Cadastro from "../public/cadastro";
-import "@react-native-async-storage/async-storage/jest/async-storage-mock";
-
 
 jest.mock("@react-native-async-storage/async-storage", () => ({
   setItem: jest.fn(),
@@ -13,7 +11,7 @@ jest.mock("@react-native-async-storage/async-storage", () => ({
 describe("Cadastro Component", () => {
   it("deve renderizar corretamente", () => {
     const { getByPlaceholderText, getByText } = render(<Cadastro />);
-    
+
     const nomeInput = getByPlaceholderText("Nome completo");
     const emailInput = getByPlaceholderText("Email");
     const confirmEmailInput = getByPlaceholderText("Confirme seu Email");
@@ -21,14 +19,49 @@ describe("Cadastro Component", () => {
     const confirmPasswordInput = getByPlaceholderText("Confirme sua senha");
     const cadastrarButton = getByText("Cadastrar");
 
-    // Simulando a entrada de dados e o clique no botão de cadastro
     fireEvent.changeText(nomeInput, "Seu Nome");
     fireEvent.changeText(emailInput, "seuemail@gmail.com");
     fireEvent.changeText(confirmEmailInput, "seuemail@gmail.com");
     fireEvent.changeText(passwordInput, "suasenha");
     fireEvent.changeText(confirmPasswordInput, "suasenha");
+
+    fireEvent.press(cadastrarButton);
+  });
+
+  it("deve exibir erro em campos obrigatórios vazios", () => {
+    const { getByText } = render(<Cadastro />);
+    const cadastrarButton = getByText("Cadastrar");
+
     fireEvent.press(cadastrarButton);
 
-    
+    // Adicione aqui as asserções para verificar se os erros são exibidos corretamente
+  });
+
+  it("deve chamar a função 'cadastrar' quando não há erros nos campos", () => {
+    const { getByPlaceholderText, getByText } = render(<Cadastro />);
+    const nomeInput = getByPlaceholderText("Nome completo");
+    const emailInput = getByPlaceholderText("Email");
+    const confirmEmailInput = getByPlaceholderText("Confirme seu Email");
+    const passwordInput = getByPlaceholderText("Senha");
+    const confirmPasswordInput = getByPlaceholderText("Confirme sua senha");
+    const cadastrarButton = getByText("Cadastrar");
+
+    fireEvent.changeText(nomeInput, "Seu Nome");
+    fireEvent.changeText(emailInput, "seuemail@gmail.com");
+    fireEvent.changeText(confirmEmailInput, "seuemail@gmail.com");
+    fireEvent.changeText(passwordInput, "suasenha");
+    fireEvent.changeText(confirmPasswordInput, "suasenha");
+
+    fireEvent.press(cadastrarButton);
+
+    // Adicione aqui as asserções para verificar se a função 'cadastrar' é chamada corretamente
+  });
+
+  it("deve exibir o Toast de sucesso", () => {
+    // Adicione aqui as asserções para verificar se o Toast de sucesso é exibido
+  });
+
+  it("deve exibir o Toast de erro", () => {
+    // Adicione aqui as asserções para verificar se o Toast de erro é exibido
   });
 });
