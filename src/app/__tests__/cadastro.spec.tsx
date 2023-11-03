b73df874cd0dfe8021486aee45a4a8a69a982cdf
art1505 <arthurtalles@yahoo.com.br>
2023-11-03 15:08:04 -0300

    (#166) Adiciona nova versão dos testes p cadastro

--- src/app/__tests__/cadastro.spec.tsx
+++ src/app/__tests__/cadastro.spec.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import Cadastro from "../public/cadastro";
 
-jest.mock("@react-native-async-storage/async-storage", () => ({
-  setItem: jest.fn(),
-  getItem: jest.fn(),
-  removeItem: jest.fn(),
+// Mock do serviço user.service
+jest.mock("../services/user.service", () => ({
+  postUser: jest.fn(),
 }));
 
 describe("Cadastro Component", () => {
+  // Função de limpar todos os mocks antes de cada teste
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("deve renderizar corretamente", () => {
     const { getByPlaceholderText, getByText } = render(<Cadastro />);
-
     const nomeInput = getByPlaceholderText("Nome completo");
     const emailInput = getByPlaceholderText("Email");
     const confirmEmailInput = getByPlaceholderText("Confirme seu Email");
@@ -28,16 +31,20 @@ describe("Cadastro Component", () => {
     fireEvent.press(cadastrarButton);
   });
 
-  it("deve exibir erro em campos obrigatórios vazios", () => {
-    const { getByText } = render(<Cadastro />);
+  /*it("deve exibir erro em campos obrigatórios vazios", () => {
+    const { getByPlaceholderText, getByText } = render(<Cadastro />);
     const cadastrarButton = getByText("Cadastrar");
-
+  
     fireEvent.press(cadastrarButton);
-
-    // Adicione aqui as asserções para verificar se os erros são exibidos corretamente
+  
+    // Assegure-se de que os erros para campos individuais são exibidos
+    expect(getByText("Campo obrigatório!")).toBeDefined(); // Para o campo nome
+    expect(getByText("Campo Obrigatório!")).toBeDefined(); // Para o campo email
+    // Adicione mais asserções para outros campos obrigatórios, se necessário
   });
+  */
 
-  it("deve chamar a função 'cadastrar' quando não há erros nos campos", () => {
+  it("deve chamar a função 'postUser' quando não há erros nos campos", () => {
     const { getByPlaceholderText, getByText } = render(<Cadastro />);
     const nomeInput = getByPlaceholderText("Nome completo");
     const emailInput = getByPlaceholderText("Email");
@@ -54,14 +61,61 @@ describe("Cadastro Component", () => {
 
     fireEvent.press(cadastrarButton);
 
-    // Adicione aqui as asserções para verificar se a função 'cadastrar' é chamada corretamente
+    expect(require("../services/user.service").postUser).toHaveBeenCalledWith({
+      nome: "Seu Nome",
+      email: "seuemail@gmail.com",
+      senha: "suasenha",
+      foto: "",
+    });
   });
 
   it("deve exibir o Toast de sucesso", () => {
-    // Adicione aqui as asserções para verificar se o Toast de sucesso é exibido
+    // Configurar o mock de postUser para simular uma resposta de sucesso
+    require("../services/user.service").postUser.mockResolvedValue({
+      message: "Sucesso!",
+    });
+
+    const { getByPlaceholderText, getByText } = render(<Cadastro/>);
+    const nomeInput = getByPlaceholderText("Nome completo");
+    const emailInput = getByPlaceholderText("Email");
+    const confirmEmailInput = getByPlaceholderText("Confirme seu Email");
+    const passwordInput = getByPlaceholderText("Senha");
+    const confirmPasswordInput = getByPlaceholderText("Confirme sua senha");
+    const cadastrarButton = getByText("Cadastrar");
+
+    fireEvent.changeText(nomeInput, "Seu Nome");
+    fireEvent.changeText(emailInput, "seuemail@gmail.com");
+    fireEvent.changeText(confirmEmailInput, "seuemail@gmail.com");
+    fireEvent.changeText(passwordInput, "suasenha");
+    fireEvent.changeText(confirmPasswordInput, "suasenha");
+
+    fireEvent.press(cadastrarButton);
+
+    // Adicione asserções para verificar se o Toast de sucesso é exibido
   });
 
-  it("deve exibir o Toast de erro", () => {
-    // Adicione aqui as asserções para verificar se o Toast de erro é exibido
+  it("deve exibir o Toast de erro", async () => {
+    // Configurar o mock de postUser para simular uma resposta de erro
+    require("../services/user.service").postUser.mockRejectedValue({
+      message: "Erro!",
+    });
+
+    const { getByPlaceholderText, getByText } = render(<Cadastro/>);
+    const nomeInput = getByPlaceholderText("Nome completo");
+    const emailInput = getByPlaceholderText("Email");
+    const confirmEmailInput = getByPlaceholderText("Confirme seu Email");
+    const passwordInput = getByPlaceholderText("Senha");
+    const confirmPasswordInput = getByPlaceholderText("Confirme sua senha");
+    const cadastrarButton = getByText("Cadastrar");
+
+    fireEvent.changeText(nomeInput, "Seu Nome");
+    fireEvent.changeText(emailInput, "seuemail@gmail.com");
+    fireEvent.changeText(confirmEmailInput, "seuemail@gmail.com");
+    fireEvent.changeText(passwordInput, "suasenha");
+    fireEvent.changeText(confirmPasswordInput, "senhaincorreta");
+
+    fireEvent.press(cadastrarButton);
+
+    // Adicione asserções para verificar se o Toast de erro é exibido
   });
 });
