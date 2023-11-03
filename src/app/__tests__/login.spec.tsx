c8efa93a1d30d6e2a0cbeb3f45f6dbd4e8945010
art1505 <arthurtalles@yahoo.com.br>
2023-11-02 23:22:11 -0300

    Adiciona testes provisórios ao login

--- src/app/__tests__/login.spec.tsx
+++ src/app/__tests__/login.spec.tsx
@@ -13,15 +13,37 @@ describe("Login Component", () => {
   it("deve renderizar corretamente", () => {
     const { getByPlaceholderText, getByText } = render(<Login />);
     
-    
     const emailInput = getByPlaceholderText("Email");
     const passwordInput = getByPlaceholderText("Senha");
     const loginButton = getByText("Entrar");
-
     
     fireEvent.changeText(emailInput, "seuemail@gmail.com");
     fireEvent.changeText(passwordInput, "suasenha");
     fireEvent.press(loginButton);
-
   });
+  
+  it("deve lidar com campos vazios e inválidos", () => {
+    const { getByPlaceholderText, getByText, queryByTestId } = render(<Login />);
+    
+    const emailInput = getByPlaceholderText("Email");
+    const passwordInput = getByPlaceholderText("Senha");
+    const loginButton = getByText("Entrar");
+    
+    // Tente efetuar login com campos vazios
+    fireEvent.press(loginButton);
+    
+    // Verifique se as mensagens de erro são exibidas
+    expect(queryByTestId("error-email")).not.toBeNull();
+    expect(queryByTestId("error-senha")).not.toBeNull();
+    
+    // Preencha campos com valores inválidos
+    fireEvent.changeText(emailInput, "email_invalido");
+    fireEvent.changeText(passwordInput, "senha_curta");
+    fireEvent.press(loginButton);
+    
+    // Verifique se as mensagens de erro correspondentes são exibidas
+    expect(queryByTestId("error-email")).not.toBeNull();
+    expect(queryByTestId("error-senha")).not.toBeNull();
+  });
+  
 });
