611e51dbb940f47a02894375066bc5741b96f0ed
Pedro Cella <pedrovscella@gmail.com>
2023-11-04 17:08:49 -0300

    (tests) Adiciona teste do BackButton

--- src/app/__tests__/BackButton.spec.tsx
+++ src/app/__tests__/BackButton.spec.tsx
@@ -8,6 +8,7 @@ jest.mock("expo-router", () => ({
   router: {
     canGoBack: jest.fn().mockReturnValue(true), // Simula que pode voltar
     back: jest.fn(),
+    push: jest.fn(),
   },
 }));
 
@@ -20,5 +21,16 @@ describe("BackButton", () => {
 
     // Verifica se a função router.back() é chamada quando o botão é pressionado
     expect(router.router.back).toHaveBeenCalled();
+  });
+
+  it("chama router.push() com a rota correta quando route é fornecido", () => {
+    const route = "pagina-anterior";
+    const { getByTestId } = render(<BackButton route={route} />);
+    const pressableElement = getByTestId("back-button-pressable");
+
+    fireEvent.press(pressableElement);
+
+    // Verifica se a função router.push() é chamada com a rota fornecida
+    expect(router.router.push).toHaveBeenCalledWith(route);
   });
 });
