401dac99997c82b63813baa8c6a44c3ed9bdd61d
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2023-11-07 00:38:31 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo","@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-    plugins: ["expo-router/babel"],
+    plugins: [
+      ["@babel/plugin-transform-class-properties", { "loose": false }],
+      ["@babel/plugin-transform-private-methods", { "loose": false }],
+    ],
   };
 };
