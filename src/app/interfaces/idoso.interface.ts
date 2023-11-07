4fda64f4bc5b350cb0da68a070886730f0bd316e
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-11-07 16:40:40 -0300

    (#168) Cria enum tipo sanguineo
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

---
+++ src/app/interfaces/idoso.interface.ts
@@ -0,0 +1,13 @@
+
+
+export enum ETipoSanguineo{
+    NENHUM = "Nenhum",
+    A_POSITIVO = "+A",
+    A_NEGATIVO = "-A",
+    B_POSITIVO = "+B",
+    B_NEGATIVO = "-B",
+    AB_POSITIVO = "+AB",
+    AB_NEGATIVO = "-AB",
+    O_POSITIVO = "+O",
+    O_NEGATIVO = "-O",
+}
