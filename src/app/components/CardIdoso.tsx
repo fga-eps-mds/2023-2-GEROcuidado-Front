66c7954ade575be6cb602a06dd28031c2ab60f1b
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-11-11 18:52:16 -0300

    (#167) Adiciona botão de editar ao CardIdoso
    Co-autohred-by: Pedro Cella <pedrovscella@gmail.com>

--- src/app/components/CardIdoso.tsx
+++ src/app/components/CardIdoso.tsx
@@ -1,15 +1,23 @@
 import React from "react"
-import { View, Image, Text, StyleSheet} from "react-native"
+import { View, Image, Text, StyleSheet, Pressable} from "react-native"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 interface IProps{
     nome : string;
+}
+
+const navigate = () => {
+    
 }
 
 export default function CardIdoso({nome}:IProps){
     return (
         <View>
             <Image source={require("../../../assets/Idoso.png")} style={styles.imagem} />
-            <Text style={styles.texto}>{nome}</Text>
+                <Text style={styles.texto}>{nome}</Text>
+                <Pressable onPress={navigate}>
+                    <Icon style={styles.foot} name="pencil" size={18} color={"black"} />
+                </Pressable>
         </View>
     )
 }
@@ -23,5 +31,9 @@ const styles = StyleSheet.create({
     texto:{
         alignSelf: "center",
     },
-    
+    foot: {
+        position: "absolute",
+        right: 3,
+        bottom: 20,
+    }
 })
