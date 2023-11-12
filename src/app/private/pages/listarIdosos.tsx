acddb9334f845dc4172f7e63e7436a77b33bbe48
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-11-12 15:45:30 -0300

    (#167) Implementa funções de delete e update idoso
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

--- src/app/private/pages/listarIdosos.tsx
+++ src/app/private/pages/listarIdosos.tsx
@@ -1,41 +1,73 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image, Pressable, ActivityIndicator } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { AntDesign } from "@expo/vector-icons";
 import BackButton from "../../components/BackButton";
 import CardIdoso from "../../components/CardIdoso";
-import CustomButton from "../../components/CustomButton";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { router } from "expo-router";
+import { IIdoso, IOrder } from "../../interfaces/idoso.interface";
+import { getAllIdoso } from "../../services/idoso.service";
+import Toast from "react-native-toast-message";
+import ModalConfirmation from "../../components/ModalConfirmation";
 
 export default function ListarIdosos() {
-  const dadosMock = [
-    {
-      nome: "Maria Dalva",
-    },
-    {
-      nome: "Sergio Gustamante",
-    },
-    {
-      nome: "Thiago Talvaneo",
-    },
-    {
-      nome: "Seu Silvio",
-    },
-    {
-      nome: "Pedro",
-    },
-    {
-      nome: "Guilherme",
-    },
-    {
-      nome: "Eren"
-    }
-  ];
+  const [idosos, setIdosos] = useState<IIdoso[]>([]);
+  const [nome, setNome] = useState("");
+  const [offset, setOffset] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [selecionado, setSelecionado] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const navigate = () => {
-    router.push({ pathname: "/private/pages/cadastrarEditarIdoso" })
+  const order: IOrder = {
+    column: "nome",
+    dir: "DESC",
+  };
+
+  const getIdosos = (
+    anterior: IIdoso[],
+    nome: string,
+    offset: number,
+  ) => {
+    setOffset(offset);
+    setNome(nome);
+
+    getAllIdoso(offset, { nome }, order)
+      .then((response) => {
+        const newIdosos = response.data as IIdoso[];
+
+        /* if (newIdosos.length === 0) {
+          setShowCarregarMais(false);
+        } */
+
+        setIdosos([...anterior, ...newIdosos]);
+      })
+      .catch((err) => {
+        const error = err as { message: string };
+        Toast.show({
+          type: "error",
+          text1: "Erro!",
+          text2: error.message,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const selecionar = () => {
+    setSelecionado(!selecionado);
+    console.log(selecionado);
   }
+
+  const navigateCadastrar = () => {
+    router.push({ pathname: "/private/pages/cadastrarIdoso" })
+  }
+
+  const confirmation = () => {
+    setModalVisible(!modalVisible)
+  }
+
+  useEffect(() => getIdosos([], "", 0), []);
 
   return (
     <View style={styles.screen}>
@@ -50,38 +82,50 @@ export default function ListarIdosos() {
 
       <View style={styles.actions}>
         <Pressable
-              style={[styles.actionButton, styles.editButton]}
-            >
-              <Text style={styles.actionButtonText}>Filtro</Text>
-            </Pressable>
+          onPress={confirmation}
+          style={[styles.actionButton, styles.editButton]}
+        >
+          <Text style={styles.actionButtonText}>Filtro</Text>
+        </Pressable>
       </View>
 
-      <View style={styles.cardIdoso}>
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          numColumns={2}
-          data={dadosMock}
-          renderItem={({ item }) => (
-            <Pressable 
-            onPress={navigate}
-            style={styles.idoso}>
-              <CardIdoso nome ={item.nome}/>
-            </Pressable>
-          )}
-        />
-      </View>
+      {loading && (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#2CCDB5" />
+        </View>
+      )}
+
+      {!loading && (
+        <View style={styles.cardIdoso}>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+            data={idosos}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={selecionar}>
+                <CardIdoso
+                  item={item} />
+              </Pressable>
+            )}
+          />
+        </View>
+      )}
 
       <View style={styles.cadastroContainer}>
-        <AntDesign name="pluscircleo" size={54} />
-        <Text style={styles.cadastroText}>Cadastrar um idoso</Text>
+        <Pressable style={styles.cadastroContainer} onPress={navigateCadastrar}>
+          <AntDesign name="pluscircleo" size={54} />
+          <Text style={styles.cadastroText}>Cadastrar um idoso</Text>
+        </Pressable>
       </View>
+
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen:{
+  screen: {
     backgroundColor: "#FFFFFF",
     height: "100%",
   },
@@ -90,7 +134,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   },
-  header:{
+  header: {
     alignSelf: "center",
     fontSize: 25,
     fontWeight: "bold",
@@ -111,10 +155,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     height: "65%",
   },
-  idoso: {
+  idosoSelecionado: {
     marginLeft: 16,
     marginRight: 16,
     marginBottom: 32,
+    borderWidth: 3,
+    borderColor: "#2CCDB5",
   },
   actionButton: {
     flexDirection: "row",
@@ -137,10 +183,20 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     marginRight: 5,
   },
-    actions: {
-      alignItems: "center",
-      width: "100%",
-      padding: 10,
-      paddingBottom: 15,
-    },
+  actions: {
+    alignItems: "center",
+    width: "100%",
+    padding: 10,
+    paddingBottom: 15,
+  },
+  loading: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    marginTop: 50,
+  },
+  idoso: {
+
+  },
 });
