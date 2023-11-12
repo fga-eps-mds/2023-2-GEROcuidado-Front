4847458504ca02ab9422c4a807fee5b95545ada7
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-11-12 15:43:31 -0300

    (#167) Adiciona funções de foto ao CardIdoso
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

--- src/app/components/CardIdoso.tsx
+++ src/app/components/CardIdoso.tsx
@@ -1,39 +1,85 @@
 import React from "react"
-import { View, Image, Text, StyleSheet, Pressable} from "react-native"
+import { View, Image, Text, StyleSheet, Pressable } from "react-native"
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { IIdoso } from "../interfaces/idoso.interface";
+import { router } from "expo-router";
 
-interface IProps{
-    nome : string;
+interface IProps {
+    item: IIdoso;
 }
 
-const navigate = () => {
-    
-}
+export default function CardIdoso({ item }: IProps) {
 
-export default function CardIdoso({nome}:IProps){
+    const getNome = (nome: string): string => {
+        return nome.length < 25 ? nome : nome.slice(0, 25) + "...";
+    };
+
+    const hasFoto = (foto: string | null | undefined) => {
+        if (!foto) return false;
+
+        const raw = foto.split("data:image/png;base64,")[1];
+        return raw.length > 0;
+    };
+
+    const getFoto = (foto: string | null | undefined) => {
+        if (hasFoto(foto)) {
+            return (
+                <Image source={{ uri: foto as string }} style={styles.imagem} />
+            );
+        }
+    }
+
+    const navigate = () => {
+        const params = { ...item, id: item.id };
+
+        router.push({
+            pathname: "/private/pages/editarIdoso",
+            params: params,
+        });
+    };
+
+
     return (
         <View>
-            <Image source={require("../../../assets/Idoso.png")} style={styles.imagem} />
-                <Text style={styles.texto}>{nome}</Text>
-                <Pressable onPress={navigate}>
-                    <Icon style={styles.foot} name="pencil" size={18} color={"black"} />
+            <Pressable style={styles.idoso}>
+                <View>
+                    <Image source={require("../../../assets/Idoso.png")} style={styles.imagem} />
+                </View>
+                <Text style={styles.texto}>{item.nome}</Text>
+                <Pressable style={styles.pencil} onPress={navigate}>
+                    <Icon style={styles.pencilIcon} name="pencil-outline" size={20} color={"white"} />
                 </Pressable>
+            </Pressable>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    imagem:{
+    imagem: {
         height: 149,
         width: 132,
         borderRadius: 12,
     },
-    texto:{
+    texto: {
         alignSelf: "center",
     },
-    foot: {
+    pencil: {
         position: "absolute",
-        right: 3,
-        bottom: 20,
-    }
+        right: -5,
+        bottom: 12,
+        backgroundColor: "#2CCDB5",
+        borderRadius: 20,
+        width: 28,
+        height: 28,
+        alignContent: "center",
+        alignItems: "center",
+    },
+    pencilIcon: {
+        marginTop: 3,
+    },
+    idoso: {
+        marginLeft: 16,
+        marginRight: 16,
+        marginBottom: 32,
+    },
 })
