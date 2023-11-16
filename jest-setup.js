d387bb46ef0aee8e4d08965e5846e0c53cea62b8
Gustavo <gugustavo137@gmail.com>
2023-11-16 10:07:00 -0300

    Adiciona pasta para mocks auxiliares de teste

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,13 @@
+import 'react-native-gesture-handler/jestSetup';
+
+module.exports = {
+  
+  moduleNameMapper: {
+    '^expo-image$': './__mocks__/expo-image.js',
+  },
+  // ... other configurations
+};
+
 jest.mock("@react-native-async-storage/async-storage", () =>
   require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
 );
