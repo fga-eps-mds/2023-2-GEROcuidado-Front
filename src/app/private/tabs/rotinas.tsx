6d45bc2ed048838c19fa29949fe38df4fd3e2905
JulioR2022 <221022041@aluno.unb.br>
2023-11-17 02:05:25 -0300

    (#191) Cria botao de criar nova rotina

--- src/app/private/tabs/rotinas.tsx
+++ src/app/private/tabs/rotinas.tsx
@@ -4,8 +4,23 @@ import { IUser } from "../../interfaces/user.interface";
 import NaoAutenticado from "../../components/NaoAutenticado";
 import EmConstrucao from "../../components/EmConstrucao";
 
+import {
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { router } from "expo-router";
+
 export default function Rotinas() {
   const [user, setUser] = useState<IUser | undefined>(undefined);
+
+  const novaRotina = () => {
+    router.push("private/pages/cadastrarRotina");
+  };
 
   const handleUser = () => {
     AsyncStorage.getItem("usuario").then((response) => {
@@ -13,8 +28,37 @@ export default function Rotinas() {
       setUser(usuario);
     });
   };
-
   useEffect(() => handleUser(), []);
 
-  return !user?.id ? <NaoAutenticado /> : <EmConstrucao />;
+  return !user?.id ? <NaoAutenticado /> : 
+  <View> 
+    <Pressable
+      style={styles.botaoCriarRotina}
+      onPress={novaRotina}
+    >
+      <Icon name="plus" color={"white"} size={20}></Icon>
+      <Text style={styles.textoBotaoCriarRotina}>Nova Rotina</Text>
+    </Pressable>
+  </View>;
+  ;
 }
+
+const styles = StyleSheet.create({
+  botaoCriarRotina: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#B4026D",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 10,
+    marginLeft: "auto",
+    marginRight: 10,
+    marginVertical: 10,
+  },
+  textoBotaoCriarRotina: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 14,
+    marginLeft: 5,
+  },
+});
