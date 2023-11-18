02f998186eaeced1d1049581e63f0f49913aefa1
art1505 <arthurtalles@yahoo.com.br>
2023-11-18 01:18:45 -0300

    (testes) Adiciona testes iniciais para listarIdoso

--- src/app/__tests__/listarIdosos.spec.tsx
+++ src/app/__tests__/listarIdosos.spec.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import ListarIdosos from '../private/pages/listarIdosos';
+// listarIdosos.spec.tsx
 
+import React from "react";
+import { render, waitFor } from "@testing-library/react-native";
+import ListarIdosos from "../private/pages/listarIdosos";
+import { getAllIdoso } from "../services/idoso.service";
 
-test('renders ListarIdosos component', () => {
-  const { getByText } = render(<ListarIdosos />);
-  const headerElement = getByText(/De quem está cuidando agora?/i);
-  expect(headerElement).toBeTruthy();
+// Mockando o módulo dos serviços para substituir as implementações
+jest.mock("../services/idoso.service");
+
+describe("ListarIdosos", () => {
+  it("deve chamar a função de getAllIdoso ao montar o componente", async () => {
+    // Simula uma resposta fictícia da API
+    (getAllIdoso as jest.Mock).mockResolvedValueOnce({ data: [] });
+
+    render(<ListarIdosos />);
+
+    // Aguarda a resolução da promessa
+    await waitFor(() => expect(getAllIdoso).toHaveBeenCalled());
+  });
+
+  it("deve exibir a lista de idosos após a conclusão da chamada da API", async () => {
+    // Simula uma resposta fictícia da API
+    (getAllIdoso as jest.Mock).mockResolvedValueOnce({ data: [{ id: 1, nome: "Idoso 1" }, { id: 2, nome: "Idoso 2" }] });
+
+    const { getByText } = render(<ListarIdosos />);
+
+    // Aguarda a resolução da promessa
+    await waitFor(() => expect(getAllIdoso).toHaveBeenCalled());
+
+    // Verifica se os elementos esperados são renderizados
+    expect(getByText("Idoso 1")).toBeTruthy();
+    expect(getByText("Idoso 2")).toBeTruthy();
+  });
+  it("deve exibir uma mensagem de erro se a chamada da API falhar", async () => {
+    const errorMessage = "Erro ao buscar idosos";
+  
+    // Simula um erro na chamada da API
+    (getAllIdoso as jest.Mock).mockRejectedValueOnce({ message: errorMessage });
+  
+    const { queryByText } = render(<ListarIdosos />);
+  
+    // Aguarda a resolução da promessa
+    await waitFor(() => expect(getAllIdoso).toHaveBeenCalled(), { timeout: 5000 });
+  
+    // Verifica se a mensagem de erro não está presente
+    expect(queryByText(errorMessage)).toBeNull();
+  });
 });
