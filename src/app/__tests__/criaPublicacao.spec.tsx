88c047963b37603bcb55e80ff689ba30ef8f1a89
Gustavo <gugustavo137@gmail.com>
2023-11-18 05:36:18 -0300

    Adiciona teste para renderizar criar publicacao

---
+++ src/app/__tests__/criaPublicacao.spec.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import AsyncStorage, { AsyncStorageStatic } from '@react-native-async-storage/async-storage'; 
+import CriaPublicacao from '../private/pages/criaPublicacao';
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(),
+}));
+
+describe('CriaPublicacao', () => {
+  test('The component rendered', () => {
+    // Mock the response for AsyncStorage.getItem
+    (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+      if (key === 'usuario') {
+        return Promise.resolve(JSON.stringify({ id: 1 }));
+      } else if (key === 'token') {
+        return Promise.resolve('mockedToken');
+      }
+      return Promise.resolve(null);
+    });
+
+    render(<CriaPublicacao />);
+  });
+});
