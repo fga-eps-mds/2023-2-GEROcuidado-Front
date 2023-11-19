9eddc5f11f7b2af49241b6494a5c6712dd4ce0f3
art1505 <arthurtalles@yahoo.com.br>
2023-11-18 23:01:58 -0300

    (teste) Adiciona novos casos de teste editarPerfil

--- src/app/__tests__/editarPerfil.spec.tsx
+++ src/app/__tests__/editarPerfil.spec.tsx
@@ -1,23 +1,84 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, fireEvent, waitFor, act } from '@testing-library/react-native';
 import EditarPerfil from '../private/pages/editarPerfil';
 
 describe('EditarPerfil component', () => {
 
   test('Atualiza nome com o input', async () => {
     const { getByPlaceholderText } = render(<EditarPerfil />);
-
     const nameInput = getByPlaceholderText('Nome completo');
-
     fireEvent.changeText(nameInput, 'Gustavo A');
 
-    // Wait for the component to re-render with the updated state
     await waitFor(() => {
-      // Check if the input field value has been updated
       expect(nameInput.props.value).toBe('Gustavo A');
     });
   });
 
-  // Addicionar mais testes
+  test('Exibe mensagem de erro ao tentar salvar com nome vazio', async () => {
+    const { getByText, getByPlaceholderText, queryByText } = render(<EditarPerfil />);
+    const nameInput = getByPlaceholderText('Nome completo');
+    fireEvent.changeText(nameInput, '');
+
+    const saveButton = getByText('Salvar');
+    fireEvent.press(saveButton);
+
+    await waitFor(() => {
+      expect(getByText('Campo obrigatório!')).toBeTruthy();
+    });
+
+    // Certifique-se de que a mensagem de erro específica não está presente quando não deve ser exibida
+    await waitFor(() => {
+      expect(queryByText('O nome completo deve ter pelo menos 5 caractéres.')).toBeNull();
+      expect(queryByText('O nome completo deve ter no máximo 60 caractéres.')).toBeNull();
+    });
+  });
+
+  test('Exibe mensagem de erro ao tentar salvar com nome muito curto', async () => {
+    const { getByText, getByPlaceholderText, findByText } = render(<EditarPerfil />);
+    const nameInput = getByPlaceholderText('Nome completo');
+    fireEvent.changeText(nameInput, 'Jo');
+
+    const saveButton = getByText('Salvar');
+    fireEvent.press(saveButton);
+
+    await act(async () => {
+      const errorMessage = await findByText('O nome completo deve ter pelo menos 5 caractéres.');
+      expect(errorMessage).toBeTruthy();
+    });
+  });
+
+  test('Exibe mensagem de erro ao tentar salvar com nome muito longo', async () => {
+    const { getByText, getByPlaceholderText, findByText } = render(<EditarPerfil />);
+    const nameInput = getByPlaceholderText('Nome completo');
+    fireEvent.changeText(
+      nameInput,
+      'Lorem Ipsum é apenas um texto fictício da indústria de impressão e composição tipográfica.'
+    );
+
+    const saveButton = getByText('Salvar');
+    fireEvent.press(saveButton);
+
+    await act(async () => {
+      const errorMessage = await findByText('O nome completo deve ter no máximo 60 caractéres.');
+      expect(errorMessage).toBeTruthy();
+    });
+  });
+
+  /*test('Exibe mensagem de sucesso ao salvar corretamente', async () => {
+    const { getByText, getByPlaceholderText, queryByText, findByText } = render(<EditarPerfil />);
+    const nameInput = getByPlaceholderText('Nome completo');
+    fireEvent.changeText(nameInput, 'Nome Válido');
+
+    const saveButton = getByText('Salvar');
+    fireEvent.press(saveButton);
+
+    // Aguarde até que o elemento "Sucesso!" seja encontrado ou a navegação seja concluída.
+    await waitFor(() => {
+      expect(findByText('Sucesso!')).toBeTruthy();
+    }, { timeout: 5000 });
+  });
+  */
+
+  // Este teste acima funciona, mas tá bugando o comando npm run test
 
 });
