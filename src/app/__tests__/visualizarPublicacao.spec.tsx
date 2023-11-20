3a8b03fdcbb6fda525d7fd65a95125404f681161
melohugo <hugoq2004@gmail.com>
2023-11-20 20:12:15 -0300

    incrementa teste da tela visualizar publicação
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>

--- src/app/__tests__/visualizarPublicacao.spec.tsx
+++ src/app/__tests__/visualizarPublicacao.spec.tsx
@@ -6,5 +6,16 @@ describe('Visualizar publicacao', () => {
   it('Rendeziza sem quebrar', () => {
     render(<VisualizarPublicacao />);
   });
+  it('displays actions correctly for admin', () => {
+    const { getByText } = render(<VisualizarPublicacao />, {
+      initialState: {
+        usuario: {
+          id: 1,
+          admin: true,
+        },
+      },
+    });
 
-});
+    expect(getByText('Apagar')).toBeTruthy();
+  });
+})
