64ed1c34387886955165a3ad2529dde0249409ea
melohugo <hugoq2004@gmail.com>
2023-11-23 22:53:27 -0300

    (#191) Adiciona teste no forum
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>

--- src/app/__tests__/forum.spec.tsx
+++ src/app/__tests__/forum.spec.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { render, waitFor } from "@testing-library/react-native";
+import { render, waitFor, fireEvent } from "@testing-library/react-native";
 import Forum from "../private/tabs/forum";
 
 describe("Forum", () => {
   it("renderiza corretamente", async () => {
     await waitFor(() => render(<Forum />));
+  });
+
+  it("should call handlePesquisar when searching", async () => {
+    const { getByPlaceholderText } = render(<Forum />);
+
+    // Wait for the component to be rendered
+    await waitFor(() => {});
+
+    // Simulate entering a search query
+    fireEvent.changeText(getByPlaceholderText("Pesquise uma publicação"), "example search");
+
+    // You might need to wait for some asynchronous actions to complete
+    await waitFor(() => {});
+
+    // Add assertions or additional tests as needed
   });
 });
