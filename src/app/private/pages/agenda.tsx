102ebd23c520679a2a11d722c773a2baa964a457
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2023-11-26 17:46:35 -0300

    Corrigindo agenda
    Co-Authored-By: melohugo <110669223+melohugo@users.noreply.github.com>
    Co-Authored-By: gabryelns <114311386+gabryelns@users.noreply.github.com>

--- src/app/private/pages/agenda.tsx
+++ src/app/private/pages/agenda.tsx
@@ -1,41 +1,120 @@
-import React, { useState } from 'react';
-import { Agenda } from 'react-native-calendars';
+import React, { useState, useEffect } from 'react';
+import { Agenda, CalendarList, LocaleConfig } from 'react-native-calendars';
 import { View, Text, StyleSheet } from 'react-native';
+import { getAllRotina } from '../../services/rotina.service';
+import { IRotina, IRotinaFilter } from '../../interfaces/rotina.interface';
+import { IIdoso } from '../../interfaces/idoso.interface';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
+import { IUser } from '../../interfaces/user.interface';
+
+interface IItem {
+  name: string;
+  dataHora: string | Date;
+}
+
+const formatDate = (date: string | Date) => {
+  if (typeof date === 'string') {
+    return date;
+  }
+
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${year}-${month}-${day}`;
+};
 
 const CalendarAgenda: React.FC = () => {
-  const [items, setItems] = useState({});
+  const [items, setItems] = useState<{ [key: string]: IItem[] }>({});
+  const [rotinas, setRotinas] = useState<IRotina[]>([]);
+  const [user, setUser] = useState<IUser | undefined>();
+  const [loading, setLoading] = useState(true);
+  const [idoso, setIdoso] = useState<IIdoso | undefined>();
 
-  const loadItems = (day: any) => {
+  const loadItems = () => {
+    const itemsData: { [key: string]: IItem[] } = {};
 
-    setItems({
-      [day.dateString]: [
-        { name: 'Passear com o idoso', time: '10:00 AM' },
-        { name: 'Dar remédio ao idoso', time: '02:30 PM' },
-        { name: 'Banhar o idoso', time: '02:30 PM' },
-        { name: 'Banhar o idoso', time: '02:30 PM' },
-        { name: 'Banhar o idoso', time: '02:30 PM' },
-        { name: 'Banhar o idoso', time: '02:30 PM' },
-        { name: 'Dormir', time: '02:30 PM' },
-        { name: 'Dormir', time: '02:30 PM' },
-        { name: 'Levantar', time: '02:30 PM' },
-      ],
+    rotinas.forEach((rotina) => {
+      const dateString = formatDate(rotina.dataHora);
+      if (!itemsData[dateString]) {
+        itemsData[dateString] = [];
+      }
+
+      itemsData[dateString].push({
+        name: rotina.titulo,
+        dataHora: rotina.dataHora,
+      });
+    });
+
+    setItems(itemsData);
+  };
+
+  const handleUser = () => {
+    AsyncStorage.getItem('usuario').then((response) => {
+      const usuario = JSON.parse(response as string);
+      setUser(usuario);
     });
   };
+
+  const getIdoso = () => {
+    AsyncStorage.getItem('idoso').then((idosoString) => {
+      if (idosoString) {
+        const idosoPayload = JSON.parse(idosoString) as IIdoso;
+        setIdoso(idosoPayload);
+      }
+    });
+  };
+
+  const getRotinas = () => {
+    setLoading(true);
+
+    const rotinaFilter: IRotinaFilter = {
+      idIdoso: Number(idoso?.id),
+    };
+
+    getAllRotina(rotinaFilter)
+      .then((response) => {
+        const newRotinas = response.data as IRotina[];
+        setRotinas(newRotinas);
+        loadItems();
+      })
+      .catch((err) => {
+        const error = err as { message: string };
+        Toast.show({
+          type: 'error',
+          text1: 'Erro!',
+          text2: error.message,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    handleUser();
+    getIdoso();
+  }, []);
+
+  useEffect(() => {
+    getRotinas();
+  }, [idoso]);
 
   const renderItem = (item: any) => (
     <View style={styles.item}>
       <Text>{item.name}</Text>
-      <Text>{item.time}</Text>
+      <Text>{item.dataHora}</Text>
     </View>
   );
 
   return (
     <Agenda
-    items={items}
-    loadItemsForMonth={loadItems}
-    renderItem={renderItem}
-    style={{ height: '100%' }} 
-  />
+      items={items}
+      loadItemsForMonth={loadItems}
+      renderItem={renderItem}
+      style={{ height: '100%' }}
+    />
   );
 };
 
