293778d40650c0ae20517b76107b0a953ad1bfea
melohugo <hugoq2004@gmail.com>
2023-11-26 10:35:13 -0300

    (#190) Adiciona teste Editar rotina
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>

---
+++ src/app/__tests__/editarRotina.spec.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react-native";
+import Rotina from "../private/pages/editarRotina";
+
+describe("Cadastrar Rotina component", () => {
+      it("renderiza corretamente", async () => {
+        await waitFor(() => render(<Rotina />));
+      });
+});
