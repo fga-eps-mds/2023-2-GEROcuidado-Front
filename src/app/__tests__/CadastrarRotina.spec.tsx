a76a2877038139f0c780f0263ba8434f84a5c30d
JulioR2022 <221022041@aluno.unb.br>
2023-11-29 00:39:24 -0300

    (#190) adiciona teste de titulo com caracteres em excesso ao cadastrar rotina

--- src/app/__tests__/CadastrarRotina.spec.tsx
+++ src/app/__tests__/CadastrarRotina.spec.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { render, waitFor } from "@testing-library/react-native";
+import { act, fireEvent, render, waitFor } from "@testing-library/react-native";
 import Rotina from "../private/pages/cadastrarRotina";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import CadastrarRotina from "../private/pages/cadastrarRotina";
 
 jest.mock("@react-native-async-storage/async-storage", () => ({
   getItem: jest.fn(),
@@ -18,5 +19,30 @@ describe("Cadastrar Rotina component", () => {
           return Promise.resolve(null);
         });
         await waitFor(() => render(<Rotina />));
+      });
+
+      it("titulo com caracteres em excesso", () => {
+        (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+          if (key === "idoso") {
+            return Promise.resolve(JSON.stringify({ id: 1 }));
+          } else if (key === "token") {
+            return Promise.resolve("mockedToken");
+          }
+          return Promise.resolve(null);
+        });
+
+        const { getByText, getByPlaceholderText } = render(<CadastrarRotina />);
+        const titulo = getByPlaceholderText("Adicionar título");
+        const salvar = getByText("Salvar");
+        
+        act(() => {
+          fireEvent.changeText(titulo,"Comer comidaaaaaaaaaaaaaaaaaaaaaa delicosaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+          fireEvent.press(salvar);
+        });
+
+        const erroTitulo = getByText("O título deve ter no máximo 100 caracteres.");
+
+        expect(erroTitulo).toBeDefined;
+
       });
 });
