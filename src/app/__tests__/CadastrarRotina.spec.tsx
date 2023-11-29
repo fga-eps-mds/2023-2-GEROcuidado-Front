7442adcf24ef8fd8af4db955f972e49762095039
JulioR2022 <221022041@aluno.unb.br>
2023-11-29 00:26:29 -0300

    (#190) melhora teste CadastrarRotina renderiza corretamente

--- src/app/__tests__/CadastrarRotina.spec.tsx
+++ src/app/__tests__/CadastrarRotina.spec.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { render, waitFor } from "@testing-library/react-native";
 import Rotina from "../private/pages/cadastrarRotina";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(),
+}));
 
 describe("Cadastrar Rotina component", () => {
       it("renderiza corretamente", async () => {
+        (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+          if (key === "idoso") {
+            return Promise.resolve(JSON.stringify({ id: 1 }));
+          } else if (key === "token") {
+            return Promise.resolve("mockedToken");
+          }
+          return Promise.resolve(null);
+        });
         await waitFor(() => render(<Rotina />));
       });
 });
