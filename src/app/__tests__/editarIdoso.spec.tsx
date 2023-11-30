1bdc09d30ef3793923bcb5b6afd99afe04d4ae02
melohugo <hugoq2004@gmail.com>
2023-11-30 11:01:27 -0300

    (#190) Adiciona teste na editar idoso

--- src/app/__tests__/editarIdoso.spec.tsx
+++ src/app/__tests__/editarIdoso.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-native/extend-expect";
-import { render, fireEvent, waitFor } from "@testing-library/react-native";
+import { render, screen, fireEvent, waitFor, act } from "@testing-library/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 import EditarIdoso from "../private/pages/editarIdoso";
@@ -38,9 +38,9 @@ describe("EditarIdoso", () => {
 
     const saveButton = getByText("Salvar");
     fireEvent.press(saveButton);
-
+    
     await waitFor(() => {
-      expect(getByText("Campo obrigatório!")).toBeTruthy();
+      expect(screen.getByText("Campo obrigatório!")).toBeTruthy();
     });
 
     // Certifique-se de que a mensagem de erro específica não está presente quando não deve ser exibida
@@ -51,6 +51,89 @@ describe("EditarIdoso", () => {
       expect(
         queryByText("O nome completo deve ter no máximo 60 caractéres."),
       ).toBeNull();
+    });
+  });
+
+
+  test("Não exibe mensagem de erro ao salvar com nome válido", async () => {
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <EditarIdoso />,
+    );
+    const nameInput = getByPlaceholderText("Nome");
+    fireEvent.changeText(nameInput, "batata");
+
+    const nameInput1 = getByPlaceholderText("Data de Nascimento");
+    fireEvent.changeText(nameInput1, "23102001");
+
+    const nameInput2 = getByPlaceholderText("Telefone Responsável");
+    fireEvent.changeText(nameInput2, "55111111111");
+    const saveButton = getByText("Salvar");
+    fireEvent.press(saveButton);
+    
+    await waitFor(() => {
+      expect(queryByText("Campo obrigatório!")).toBeNull();
+      expect(
+        queryByText("O nome completo deve ter pelo menos 5 caractéres."),
+      ).toBeNull();
+      expect(
+        queryByText("O nome completo deve ter no máximo 60 caractéres."),
+      ).toBeNull();
+    });
+  });
+
+  test("Exibe mensagem de erro ao tentar salvar com curto", async () => {
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <EditarIdoso />,
+    );
+    const nameInput = getByPlaceholderText("Nome");
+    fireEvent.changeText(nameInput, "oi");
+
+    const nameInput1 = getByPlaceholderText("Data de Nascimento");
+    fireEvent.changeText(nameInput1, "23102001");
+
+    const nameInput2 = getByPlaceholderText("Telefone Responsável");
+    fireEvent.changeText(nameInput2, "55111111111");
+
+    const saveButton = getByText("Salvar");
+    fireEvent.press(saveButton);
+    await waitFor(() => {
+      expect(screen.getByText("O nome completo deve ter pelo menos 5 caractéres.")).toBeDefined();
+    });
+    // Certifique-se de que a mensagem de erro específica não está presente quando não deve ser exibida
+    await waitFor(() => {
+      expect(
+        queryByText("Campo obrigatório!"),
+      ).toBeNull();
+      expect(
+        queryByText("O nome completo deve ter no máximo 60 caractéres."),
+      ).toBeNull();
+    });
+  });
+
+  test("Exibe mensagem de erro ao tentar salvar com nome muito longo", async () => {
+    const { getByText, getByPlaceholderText, findByText } = render(
+      <EditarIdoso />,
+    );
+    const nameInput = getByPlaceholderText("Nome");
+    fireEvent.changeText(
+      nameInput,
+      "Lorem Ipsum é apenas um texto fictício da indústria de impressão e composição tipográfica.",
+    );
+
+    const nameInput1 = getByPlaceholderText("Data de Nascimento");
+    fireEvent.changeText(nameInput1, "23102001");
+
+    const nameInput2 = getByPlaceholderText("Telefone Responsável");
+    fireEvent.changeText(nameInput2, "55111111111");
+
+    const saveButton = getByText("Salvar");
+    fireEvent.press(saveButton);
+
+    await act(async () => {
+      const errorMessage = await findByText(
+        "O nome completo deve ter no máximo 60 caractéres.",
+      );
+      expect(errorMessage).toBeTruthy();
     });
   });
 });
