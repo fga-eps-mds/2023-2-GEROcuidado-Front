d64564ee048bf7484282d27070c0076b891f54ff
JulioR2022 <221022041@aluno.unb.br>
2023-11-30 14:04:21 -0300

    (#190) adiciona teste do arquivo CadastrarRotina
    Co-authored-by: gabryelns <gabryelnick91@gmail.com>

--- src/app/__tests__/CadastrarRotina.spec.tsx
+++ src/app/__tests__/CadastrarRotina.spec.tsx
@@ -3,6 +3,7 @@ import { act, fireEvent, render, waitFor } from "@testing-library/react-native";
 import Rotina from "../private/pages/cadastrarRotina";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import CadastrarRotina from "../private/pages/cadastrarRotina";
+import { ECategoriaRotina } from "../interfaces/rotina.interface";
 
 jest.mock("@react-native-async-storage/async-storage", () => ({
   getItem: jest.fn(),
@@ -43,6 +44,69 @@ describe("Cadastrar Rotina component", () => {
         const erroTitulo = getByText("O título deve ter no máximo 100 caracteres.");
 
         expect(erroTitulo).toBeDefined;
+
+      });
+
+      it("lidar com descricao com caracteres em excesso", () => {
+        (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+          if (key === "idoso") {
+            return Promise.resolve(JSON.stringify({ id: 1 }));
+          } else if (key === "token") {
+            return Promise.resolve("mockedToken");
+          }
+          return Promise.resolve(null);
+        });
+
+        const { getByText, getByPlaceholderText } = render(<CadastrarRotina />);
+        const titulo = getByPlaceholderText("Adicionar título");
+        const salvar = getByText("Salvar");
+        const dataRotina = getByPlaceholderText("Data da rotina");
+        const horaRotina = getByPlaceholderText("Horário de início");
+        const descricaoRotina = getByPlaceholderText("Descrição");
+        
+        act(() => {
+          fireEvent.changeText(titulo,"Comer comida");
+          fireEvent.changeText(dataRotina,"30/11/2023");
+          fireEvent.changeText(horaRotina,"13:45");
+   
+          fireEvent.changeText(descricaoRotina,"comer é bomComer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer comer ");
+          fireEvent.press(salvar);
+        });
+
+        const erroDescricao = getByText("A descrição deve ter no máximo 300 caracteres.");
+
+        expect(erroDescricao).toBeDefined;
+
+      });
+
+      it("lidar com descricao com caracteres em excesso", () => {
+        (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+          if (key === "idoso") {
+            return Promise.resolve(JSON.stringify({ id: 1 }));
+          } else if (key === "token") {
+            return Promise.resolve("mockedToken");
+          }
+          return Promise.resolve(null);
+        });
+
+        const { getByText, getByPlaceholderText } = render(<CadastrarRotina />);
+        const titulo = getByPlaceholderText("Adicionar título");
+        const salvar = getByText("Salvar");
+        const dataRotina = getByPlaceholderText("Data da rotina");
+        const horaRotina = getByPlaceholderText("Horário de início");
+        const descricaoRotina = getByPlaceholderText("Descrição");
+        
+        act(() => {
+          fireEvent.changeText(titulo,"Comer comida");
+          fireEvent.changeText(dataRotina,"30/11/2023");
+          fireEvent.changeText(horaRotina,"111:455");
+          fireEvent.changeText(descricaoRotina,"comer");
+          fireEvent.press(salvar);
+        });
+        
+        const erroHora = getByText("Hora deve ser no formato hh:mm!");
+
+        expect(erroHora).toBeDefined;
 
       });
 });
