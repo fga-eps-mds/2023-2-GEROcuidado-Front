17add1bfde5e243ed7361fce657ff511163e8d74
gabryelns <gabryelnick91@gmail.com>
2023-11-30 15:08:49 -0300

    (#190) adiciona teste descricao excedida
    Co-authored-by: melohugo <hugoq2004@gmail.com>
    Co-authored-by: JulioR2022 <221022041@aluno.unb.br>

--- src/app/__tests__/criaPublicacao.spec.tsx
+++ src/app/__tests__/criaPublicacao.spec.tsx
@@ -37,3 +37,30 @@ test('lidar com titulo com caracteres em excesso', () => {
     expect(erroTitulo).toBeDefined();
 
   });
+
+  test('lidar com descricao com caracteres em excesso', () => {
+    (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+      if (key === "usuario") {
+        return Promise.resolve(JSON.stringify({ id: 1 }));
+      } else if (key === "token") {
+        return Promise.resolve("mockedToken");
+      }
+      return Promise.resolve(null);
+    });
+    
+    const { getByText, getByPlaceholderText } = render(<CriaPublicacao />);
+    const descricao = getByPlaceholderText("Descrição");
+    const titulo = getByPlaceholderText("Título");
+    const publicarButton = getByText("Publicar");
+  
+    act(() => {
+      fireEvent.changeText(descricao,"Uma gota de chuva dança na folha de uma árvore, enquanto o sol se despede no horizonte. Crianças riem, pássaros entoam melodias e o aroma de flores enche o ar. Num café, histórias se entrelaçam entre xícaras de café fumegante. Carros traçam linhas na estrada, guiando-se pela sinfonia do tráfego. No canto de uma biblioteca, páginas de livros sussurram segredos do passado. Um artista na praça pinta a vida em telas coloridas, capturando momentos efêmeros. No silêncio da noite, estrelas cintilam, revelando um cosmos vasto e misterioso. Em algum lugar, o som suave do mar acalma mentes inquietas. No caos do quotidiano, a vida tece sua tapeçaria, entrelaçando experiências únicas. E assim, o mundo gira, ecoando uma sinfonia interminável de histórias, cada uma com sua própria magia.");
+      fireEvent.changeText(titulo,"Em uma cidade movimentada, as luzes da noite pintam um espetáculo urbano. Carros traçam caminhos, pessoas dançam na rua, e a cidade nunca dorme. ");
+      fireEvent.press(publicarButton);
+    });
+  
+      const erroDescricao = getByText("Deve ter no máximo 500 caracteres!");
+  
+      expect(erroDescricao).toBeDefined();
+  
+    });
