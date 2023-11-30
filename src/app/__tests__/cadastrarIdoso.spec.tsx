40de492948b5d639faf7a19890338eab792a4259
Thefast777 <221021984@aluno.unb.br>
2023-11-30 11:41:50 -0300

    (#190) adiciona teste da quantidade maxima de caracteres no cadastro do nome do idoso
    Co-authored-by: melohugo <hugoq2004@gmail.com>

--- src/app/__tests__/cadastrarIdoso.spec.tsx
+++ src/app/__tests__/cadastrarIdoso.spec.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import CadastrarIdoso from '../private/pages/cadastrarIdoso';
-import AsyncStorage, { AsyncStorageStatic } from '@react-native-async-storage/async-storage';
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+import CadastrarIdoso from "../private/pages/cadastrarIdoso";
+import AsyncStorage, {
+  AsyncStorageStatic,
+} from "@react-native-async-storage/async-storage";
 
 // Mock any dependencies if needed
 // Mock AsyncStorage
-jest.mock('@react-native-async-storage/async-storage', () => ({
-    getItem: jest.fn(),
-  }));
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(),
+}));
 
-describe('CadastrarIdoso component', () => {
-  test('renders correctly', () => {
-
+describe("CadastrarIdoso component", () => {
+  test("renders correctly", () => {
     (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
-        if (key === 'usuario') {
-          return Promise.resolve(JSON.stringify({ id: 1 }));
-        } else if (key === 'token') {
-          return Promise.resolve('mockedToken');
-        }
-        return Promise.resolve(null);
-      });
+      if (key === "usuario") {
+        return Promise.resolve(JSON.stringify({ id: 1 }));
+      } else if (key === "token") {
+        return Promise.resolve("mockedToken");
+      }
+      return Promise.resolve(null);
+    });
 
     const { getByText } = render(<CadastrarIdoso />);
-    
-    const cadastrarButton = getByText('Cadastrar');
+
+    const cadastrarButton = getByText("Cadastrar");
     expect(cadastrarButton).toBeTruthy();
   });
 
@@ -42,21 +43,44 @@ describe('CadastrarIdoso component', () => {
 
     const saveButton = getByText("Cadastrar");
     fireEvent.press(saveButton);
-    
+
     await waitFor(() => {
-      expect(getByText("O nome completo deve ter pelo menos 5 caractéres.")).toBeTruthy();
+      expect(
+        getByText("O nome completo deve ter pelo menos 5 caractéres."),
+      ).toBeTruthy();
     });
 
     // Certifique-se de que a mensagem de erro específica não está presente quando não deve ser exibida
     await waitFor(() => {
-      expect(
-        queryByText("Campo obrigatório!"),
-      ).toBeNull();
+      expect(queryByText("Campo obrigatório!")).toBeNull();
       expect(
         queryByText("O nome completo deve ter no máximo 60 caractéres."),
       ).toBeNull();
     });
   });
 
-  
+  test("Exibe menagem de erro ao tentar cadastrar idoso com uma quantidade de caracteres maior do que o permitido", async () => {
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <CadastrarIdoso />,
+    );
+    const nameInput = getByPlaceholderText("Nome");
+    fireEvent.changeText(
+      nameInput,
+      "extrapolando a quantidade de erros para consguir ultrapassar o numero de 60 caracacteres",
+    );
+
+    const nameInput1 = getByPlaceholderText("Data de Nascimento");
+    fireEvent.changeText(nameInput1, "23102001");
+
+    const nameInput2 = getByPlaceholderText("Telefone Responsável");
+    fireEvent.changeText(nameInput2, "55111111111");
+
+    const saveButton = getByText("Cadastrar");
+    fireEvent.press(saveButton);
+
+    await waitFor(() => {
+      expect(
+        queryByText("O nome completo deve ter no máximo 60 caractéres.")).toBeTruthy();
+    });
+  });
 });
