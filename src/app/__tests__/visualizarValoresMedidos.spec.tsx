c26543b7129d969e43dd4bbd2ee6358ff55c1325
gabryelns <gabryelnick91@gmail.com>
2023-11-30 23:29:47 -0300

    (#213) adiciona teste tela ver valores medidos

---
+++ src/app/__tests__/visualizarValoresMedidos.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import VisualizarValoresMedidos from '../private/pages/visualizarValoresMedidos';
+
+describe('Visualizar Valores Medidos Component', () => {
+
+    test('The component rendered', () => {
+        (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
+          if (key === 'usuario') {
+            return Promise.resolve(JSON.stringify({ id: 1 }));
+          } else if (key === 'token') {
+            return Promise.resolve('mockedToken');
+          }
+          return Promise.resolve(null);
+        });
+    
+        render(<VisualizarValoresMedidos />);
+      });
+});
