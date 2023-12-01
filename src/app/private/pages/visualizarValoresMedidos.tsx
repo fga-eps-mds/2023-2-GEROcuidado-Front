a285ab1669ea8f81d1f1d9ec82e6efdd6c3bb6e9
gabryelns <gabryelnick91@gmail.com>
2023-11-30 23:45:48 -0300

    (#213) adiciona botao novo valor a tela

--- src/app/private/pages/visualizarValoresMedidos.tsx
+++ src/app/private/pages/visualizarValoresMedidos.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { IUser } from "../../interfaces/user.interface";
 import NaoAutenticado from "../../components/NaoAutenticado";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Pressable, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function VisualizarValoresMedidos() {
   const [user, setUser] = useState<IUser | undefined>(undefined);
@@ -15,9 +16,24 @@ export default function VisualizarValoresMedidos() {
 
   useEffect(() => handleUser(), []);
 
+  const novoValor = () => {
+
+  };
+
   return !user?.id ? <NaoAutenticado /> : (
-    <View style = {styles.header}> </View>
-  )
+    <View>
+    <View>
+    <View style={styles.header}>
+
+    </View>
+
+      <Pressable style={styles.botaoNovoValor} onPress={novoValor}>
+        <Icon name="plus" color={"white"} size={20} />
+        <Text style={styles.textoBotaoNovoValor}>Novo Valor</Text>
+      </Pressable>
+    </View>
+    </View>
+  );
   }
 
   const styles = StyleSheet.create({
@@ -28,5 +44,24 @@ export default function VisualizarValoresMedidos() {
     padding: 57,
     flexDirection: "row",
     alignItems: "center",
+  },
+
+  botaoNovoValor: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#B4026D",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 10,
+    marginLeft: "auto",
+    marginRight: 10,
+    marginVertical: 10,
+  },
+  
+  textoBotaoNovoValor: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 14,
+    marginLeft: 5,
   },
   });
