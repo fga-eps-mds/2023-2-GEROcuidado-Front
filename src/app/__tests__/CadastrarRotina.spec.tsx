cc65cd65637689cfbda313debd86c0f785633444
melohugo <hugoq2004@gmail.com>
2023-12-01 19:25:45 -0300

    (#202) Adiciona teste com titulo muito grande em cadastrar rotina
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>

--- src/app/__tests__/CadastrarRotina.spec.tsx
+++ src/app/__tests__/CadastrarRotina.spec.tsx
@@ -18,5 +18,20 @@ describe('CadastrarRotina Component', () => {
         const erroHora = getByTestId('Erro-titulo');
     
         expect(erroHora.props.children.props.text).toBe('Campo obrigatório!');
+    });
+
+    it('Salvar com titulo muito grande', async () => {
+        const { getByText, getByPlaceholderText, getByTestId } = render(<CadastrarRotina />);
+              
+        const titulo = getByPlaceholderText('Adicionar título');
+        const salvar = getByText('Salvar');
+
+        act(() => {
+            fireEvent.changeText(titulo, 'Por que o livro de matemática está sempre triste? Porque tem muitos problemas! hahahahahahhahahahahhahahaahahahahahahhahahahahahahahahahahahhahaahahahahahahahahah');
+            fireEvent.press(salvar);
         });
-      });
+        const erroHora = getByText('O título deve ter no máximo 100 caracteres.');
+    
+        expect(erroHora).toBeTruthy();
+    });
+});
