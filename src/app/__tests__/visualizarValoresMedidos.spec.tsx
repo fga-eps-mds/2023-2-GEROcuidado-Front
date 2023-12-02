6060d5838b151141f1d95eff25c963c53f197b07
art1505 <arthurtalles@yahoo.com.br>
2023-12-02 02:13:16 -0300

    (#213) Corrige teste para a tela

--- src/app/__tests__/visualizarValoresMedidos.spec.tsx
+++ src/app/__tests__/visualizarValoresMedidos.spec.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import VisualizarValoresMedidos from '../private/pages/visualizarValoresMedidos';
 
 describe('Visualizar Valores Medidos Component', () => {
-
     test('The component rendered', () => {
         (AsyncStorage.getItem as jest.Mock).mockImplementation((key) => {
-          if (key === 'usuario') {
-            return Promise.resolve(JSON.stringify({ id: 1 }));
-          } else if (key === 'token') {
-            return Promise.resolve('mockedToken');
-          }
-          return Promise.resolve(null);
+            if (key === 'usuario') {
+                return Promise.resolve(JSON.stringify({ id: 1 }));
+            } else if (key === 'token') {
+                return Promise.resolve('mockedToken');
+            }
+            return Promise.resolve(null);
         });
-    
-        render(<VisualizarValoresMedidos />);
-      });
+        render(
+            <NavigationContainer>
+                <VisualizarValoresMedidos />
+            </NavigationContainer>
+        );
+    });
 });
