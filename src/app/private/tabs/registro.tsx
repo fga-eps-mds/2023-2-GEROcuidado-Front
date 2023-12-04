ff35326a900d881101f084bb9e15a561a72fd657
art1505 <arthurtalles@yahoo.com.br>
2023-12-04 15:07:11 -0300

    (#205) adiciona versao inicial tela de registro
    Co-authored-by: gabryelns <gabryelnick91@gmail.com>

---
+++ src/app/private/tabs/registro.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { IUser } from "../../interfaces/user.interface";
+import NaoAutenticado from "../../components/NaoAutenticado";
+import EmConstrucao from "../../components/EmConstrucao";
+import { View, StyleSheet } from "react-native";
+
+export default function Registros() {
+  const [user, setUser] = useState<IUser | undefined>(undefined);
+
+  const handleUser = () => {
+    AsyncStorage.getItem("usuario").then((response) => {
+      const usuario = JSON.parse(response as string);
+      setUser(usuario);
+    });
+  };
+
+  useEffect(() => handleUser(), []);
+
+  return !user?.id ? <NaoAutenticado /> : (
+    <View style = {styles.header}>
+    
+    </View>
+  )
+  };
+
+  const styles = StyleSheet.create({
+
+  header: {
+    backgroundColor: "#2CCDB5",
+    width: "100%",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  });
