09886838cb5462c80d26dc16539139efba351e9a
art1505 <arthurtalles@yahoo.com.br>
2023-12-04 14:22:50 -0300

    (#116) Adiciona correção de teste

--- src/app/__tests__/indicaCategoriaMetrica.spec.tsx
+++ src/app/__tests__/indicaCategoriaMetrica.spec.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, waitFor, fireEvent } from '@testing-library/react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import CriarMetrica from '../private/pages/indicaCategoriaMetrica';
 
+
 describe('CriarMetrica Component', () => {
   beforeEach(async () => {
-
     await AsyncStorage.setItem('usuario', JSON.stringify({ id: '123', nome: 'Nome do idoso' }));
   });
 
   afterEach(async () => {
-
     await AsyncStorage.removeItem('usuario');
   });
 
   it('displays user information and metric cards when authenticated', async () => {
-    const { getByTestId, getByText } = render(<CriarMetrica />);
-  
+    const { findByText, findByTestId, debug } = render(<CriarMetrica />);
+
+    debug();
 
     await waitFor(() => {
-      expect(getByText('Nome do idoso')).toBeTruthy();
-      
-  
-      expect(getByText('Nova Métrica')).toBeTruthy();
-      
-      expect(getByTestId('frequenciaCardiaca-card')).toBeTruthy();
+      expect(findByText('Nome do idoso')).toBeTruthy();
+      expect(findByText('Nova Métrica')).toBeTruthy();
+      expect(findByTestId('frequenciaCardiaca-card')).toBeTruthy();
+    }, { timeout: 10000 }); 
 
-    });
-  });
   
+  });
+
+
 });
