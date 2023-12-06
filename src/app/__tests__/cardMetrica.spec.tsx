ba330cac3d392c607010e3ca4961e432f9dfa242
gabryelns <gabryelnick91@gmail.com>
2023-12-06 02:41:25 -0300

    (#116) adiciona teste total corrigido cardrotina

--- src/app/__tests__/cardMetrica.spec.tsx
+++ src/app/__tests__/cardMetrica.spec.tsx
@@ -6,8 +6,8 @@ import { EMetricas } from "../interfaces/metricas.interface";
 import CardMetrica from "../components/CardMetrica";
 
 const mockItem = {
-    id: 1, // Adicione um valor adequado
-  idIdoso: 123, // Adicione a propriedade idIdoso
+    id: 1, 
+  idIdoso: 123, 
   nome: 'João Silva',
   dataNascimento: '1950-01-01',
   idUsuario: 123,
@@ -20,7 +20,86 @@ const mockItem = {
   valor: 75,
  
  };
+
+ const mockItem1 = {
+  id: 1, 
+  idIdoso: 123,
+nome: 'João Silva',
+dataNascimento: '1950-01-01',
+idUsuario: 123,
+foto: 'url_da_foto.jpg',
+tipoSanguineo: ETipoSanguineo.A_POSITIVO,
+telefoneResponsavel: '123456789',
+descricao: 'Idoso com histórico de hipertensão',
+dataHora: new Date(),
+categoria: EMetricas.GLICEMIA,
+valor: 75,
+
+};
  
+const mockItem2 = {
+  id: 1, 
+  idIdoso: 123,
+nome: 'João Silva',
+dataNascimento: '1950-01-01',
+idUsuario: 123,
+foto: 'url_da_foto.jpg',
+tipoSanguineo: ETipoSanguineo.A_POSITIVO,
+telefoneResponsavel: '123456789',
+descricao: 'Idoso com histórico de hipertensão',
+dataHora: new Date(),
+categoria: EMetricas.PESO,
+valor: 75,
+
+};
+
+const mockItem3 = {
+  id: 1, 
+  idIdoso: 123,
+nome: 'João Silva',
+dataNascimento: '1950-01-01',
+idUsuario: 123,
+foto: 'url_da_foto.jpg',
+tipoSanguineo: ETipoSanguineo.A_POSITIVO,
+telefoneResponsavel: '123456789',
+descricao: 'Idoso com histórico de hipertensão',
+dataHora: new Date(),
+categoria: EMetricas.PRESSAO_SANGUINEA,
+valor: 75,
+
+};
+
+const mockItem4 = {
+  id: 1, 
+idIdoso: 123, 
+nome: 'João Silva',
+dataNascimento: '1950-01-01',
+idUsuario: 123,
+foto: 'url_da_foto.jpg',
+tipoSanguineo: ETipoSanguineo.A_POSITIVO,
+telefoneResponsavel: '123456789',
+descricao: 'Idoso com histórico de hipertensão',
+dataHora: new Date(),
+categoria: EMetricas.SATURACAO_OXIGENIO,
+valor: 75,
+
+};
+
+const mockItem5 = {
+  id: 1, 
+idIdoso: 123, 
+nome: 'João Silva',
+dataNascimento: '1950-01-01',
+idUsuario: 123,
+foto: 'url_da_foto.jpg',
+tipoSanguineo: ETipoSanguineo.A_POSITIVO,
+telefoneResponsavel: '123456789',
+descricao: 'Idoso com histórico de hipertensão',
+dataHora: new Date(),
+categoria: EMetricas.TEMPERATURA,
+valor: 75,
+
+};
 
  jest.mock("expo-router", () => ({
     router: {
@@ -39,6 +118,52 @@ describe('CardIdoso Component', () => {
       expect(getByText('bpm')).toBeTruthy();
     } else {
       expect(() => getByText('bpm')).toThrow();
+    }
+  });
+  it('exibe as unidades corretas 1', () => {
+    const { getByText } = render(<CardMetrica item={mockItem1} />);
+    
+    if (mockItem1.categoria === EMetricas.GLICEMIA) {
+      expect(getByText('mg/dL')).toBeTruthy();
+    } else {
+      expect(() => getByText('mg/dL')).toThrow();
+    }
+  });
+  it('exibe as unidades corretas 2', () => {
+    const { getByText } = render(<CardMetrica item={mockItem2} />);
+    
+    if (mockItem2.categoria === EMetricas.PESO) {
+      expect(getByText('kg')).toBeTruthy();
+    } else {
+      expect(() => getByText('kg')).toThrow();
+    }
+  });
+  it('exibe as unidades corretas 3', () => {
+    const { getByText } = render(<CardMetrica item={mockItem3} />);
+    
+    if (mockItem3.categoria === EMetricas.PRESSAO_SANGUINEA) {
+      expect(getByText('mmHg')).toBeTruthy();
+    } else {
+      expect(() => getByText('mmHg')).toThrow();
+    }
+  });
+  it('exibe as unidades corretas 4', () => {
+    const { getByText } = render(<CardMetrica item={mockItem4} />);
+    
+    if (mockItem4.categoria === EMetricas.SATURACAO_OXIGENIO) {
+      expect(getByText('%')).toBeTruthy();
+    } else {
+      expect(() => getByText('%')).toThrow();
+    }
+  });
+
+  it('exibe as unidades corretas 5', () => {
+    const { getByText } = render(<CardMetrica item={mockItem5} />);
+    
+    if (mockItem5.categoria === EMetricas.TEMPERATURA) {
+      expect(getByText('°C')).toBeTruthy();
+    } else {
+      expect(() => getByText('°C')).toThrow();
     }
   });
 
