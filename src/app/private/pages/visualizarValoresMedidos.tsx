42928b9a896ac6a259888582ae073fee0ac571d5
art1505 <arthurtalles@yahoo.com.br>
2023-12-06 23:48:51 -0300

    (#116) Adiciona base para visualizar dados

--- src/app/private/pages/visualizarValoresMedidos.tsx
+++ src/app/private/pages/visualizarValoresMedidos.tsx
@@ -4,9 +4,14 @@ import { IUser } from "../../interfaces/user.interface";
 import NaoAutenticado from "../../components/NaoAutenticado";
 import { View, StyleSheet, Pressable, Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from '@react-navigation/native';
 
 export default function VisualizarValoresMedidos() {
   const [user, setUser] = useState<IUser | undefined>(undefined);
+  const [selectedMetric, setSelectedMetric] = useState<string | undefined>(undefined);
+  const [hasData, setHasData] = useState<boolean>(false); 
+  const navigation = useNavigation();
+
   const handleUser = () => {
     AsyncStorage.getItem("usuario").then((response) => {
       const usuario = JSON.parse(response as string);
@@ -14,36 +19,76 @@ export default function VisualizarValoresMedidos() {
     });
   };
 
-  useEffect(() => handleUser(), []);
+  useEffect(() => {
+    handleUser();
+
+    AsyncStorage.getItem('selectedMetric').then((metric) => {
+      setSelectedMetric(metric || "");
+    });
+
+    const temDados = false; 
+    setHasData(temDados);
+  }, []);
 
   const novoValor = () => {
 
   };
 
-  return !user?.id ? <NaoAutenticado /> : (
-    <View>
-    <View>
-    <View style={styles.header}>
+  const apagarMetrica = () => {
 
-    </View>
+  };
+
+  return !user?.id ? <NaoAutenticado /> : (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Pressable onPress={() => navigation.goBack()} style={styles.iconContainer}>
+          <Icon name="chevron-left" size={20} color="white" />
+        </Pressable>
+        <Text style={styles.headerText}>{selectedMetric}</Text> {/*Substituir lógica tual de puxar o título/categoria da métrica*/}
+      </View>
 
       <Pressable style={styles.botaoNovoValor} onPress={novoValor}>
         <Icon name="plus" color={"white"} size={20} />
         <Text style={styles.textoBotaoNovoValor}>Novo Valor</Text>
       </Pressable>
-    </View>
+
+      {hasData ? (
+
+        <View>
+          {/* ... outros componentes relacionados aos dados cadastrados */}
+        </View>
+      ) : (
+        // Conteúdo para nenhum dado cadastrado
+        <Text style={styles.nenhumDado}>Nenhum dado cadastrado</Text>
+      )}
+
+      <Pressable style={styles.apagarMetrica} onPress={apagarMetrica}>
+        <Text style={styles.textoApagarMetrica}>Apagar Métrica</Text>
+      </Pressable>
     </View>
   );
-  }
+}
 
-  const styles = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   header: {
     backgroundColor: "#2CCDB5",
     width: "100%",
-    padding: 57,
+    padding: 20,
     flexDirection: "row",
     alignItems: "center",
+  },
+  iconContainer: {
+    marginRight: 10,
+  },
+  headerText: {
+    flex: 1,
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
   },
 
   botaoNovoValor: {
@@ -64,4 +109,24 @@ export default function VisualizarValoresMedidos() {
     fontSize: 14,
     marginLeft: 5,
   },
-  });
+
+  nenhumDado: {
+    color: '#D3D3D3',
+    fontSize: 18,
+    fontWeight:"bold",
+    textAlign: 'center',
+    marginTop: 40,
+  },
+
+  apagarMetrica: {
+    position: 'absolute', 
+    bottom: 60, 
+    width: '100%', 
+    alignItems: 'center', 
+  },
+  textoApagarMetrica: {
+    color: '#FF7D7D',
+    fontSize: 18, 
+    fontWeight: '400', 
+  },
+});
