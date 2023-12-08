524a663fb111834f33dc829e77e73902496db8a3
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-12-08 17:26:30 -0300

    (#116) Atualiza CardMetrica
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

--- src/app/components/CardMetrica.tsx
+++ src/app/components/CardMetrica.tsx
@@ -1,77 +1,137 @@
 
 import React from "react";
-import { View, Text, StyleSheet, Pressable, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import { router } from "expo-router";
-import { getImageUri, noImage } from "../shared/helpers/image.helper";
-import { Image } from "expo-image";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { EMetricas, IMetrica } from "../interfaces/metricas.interface";
+import { EMetricas, IMetrica, IMetricaValueFilter, IOrder, IValorMetrica } from "../interfaces/metricas.interface";
+import { getAllMetricaValues } from "../services/metricaValue.service";
+import Toast from "react-native-toast-message";
+import { Entypo } from "@expo/vector-icons";
 
 interface IProps {
     item: IMetrica;
 }
 
 export default function CardMetrica({ item }: IProps) {
+    const [valorMetrica, setValorMetrica] = useState<IValorMetrica>();
+    const [dataHora, setDataHora] = useState<string>()
+    const [hora, setHora] = useState("");
+
+    const order: IOrder = {
+        column: "dataHora",
+        dir: "DESC",
+    }
 
     const titleColor = '#000'
-    const textColor = '#888' 
+    const textColor = '#888'
 
     const unidade = () => {
-        if(item.categoria == EMetricas.FREQ_CARDIACA){
+        if (item.categoria == EMetricas.FREQ_CARDIACA) {
             return "bpm"
         }
-        if(item.categoria == EMetricas.GLICEMIA){
+        if (item.categoria == EMetricas.GLICEMIA) {
             return "mg/dL"
         }
-        if(item.categoria == EMetricas.PESO){
+        if (item.categoria == EMetricas.PESO) {
             return "kg"
         }
-        if(item.categoria == EMetricas.PRESSAO_SANGUINEA){
+        if (item.categoria == EMetricas.PRESSAO_SANGUINEA) {
             return "mmHg"
         }
-        if(item.categoria == EMetricas.SATURACAO_OXIGENIO){
+        if (item.categoria == EMetricas.SATURACAO_OXIGENIO) {
             return "%"
         }
-        if(item.categoria == EMetricas.TEMPERATURA){
+        if (item.categoria == EMetricas.TEMPERATURA) {
             return "°C"
+        }
+        if (item.categoria == EMetricas.HORAS_DORMIDAS) {
+            return "h"
+        }
+        if (item.categoria == EMetricas.ALTURA) {
+            return "cm"
+        }
+        if (item.categoria == EMetricas.IMC) {
+            return "kg/m²"
         }
     }
 
     const icone = () => {
-        if(item.categoria == EMetricas.FREQ_CARDIACA){
-            return <FontAwesome name="heartbeat" color={"#FF7D7D"} size={25}/>
+        if (item.categoria == EMetricas.FREQ_CARDIACA) {
+            return <FontAwesome name="heartbeat" color={"#FF7D7D"} size={25} />
         }
-        if(item.categoria == EMetricas.GLICEMIA){
-            return <FontAwesome name="cubes" color={"#3F3F3F"} size={25}/>
+        if (item.categoria == EMetricas.GLICEMIA) {
+            return <FontAwesome name="cubes" color={"#3F3F3F"} size={25} />
         }
-        if(item.categoria == EMetricas.PESO){
-            return <Icon name="scale-bathroom" color={"#B4026D"} size={25}/>
+        if (item.categoria == EMetricas.PESO) {
+            return <Icon name="scale-bathroom" color={"#B4026D"} size={25} />
         }
-        if(item.categoria == EMetricas.PRESSAO_SANGUINEA){
-            return <FontAwesome name="tint" color={"#FF7D7D"} size={25}/>
+        if (item.categoria == EMetricas.PRESSAO_SANGUINEA) {
+            return <FontAwesome name="tint" color={"#FF7D7D"} size={25} />
         }
-        if(item.categoria == EMetricas.SATURACAO_OXIGENIO){
-            return <View><Text>O<Text style={{fontSize:10}}>2</Text></Text></View>
+        if (item.categoria == EMetricas.SATURACAO_OXIGENIO) {
+            return <View><Text>O<Text style={{ fontSize: 10 }}>2</Text></Text></View>
         }
-        if(item.categoria == EMetricas.TEMPERATURA){
-            return <FontAwesome name="thermometer" color={"#FFAC7D"} size={25}/>
+        if (item.categoria == EMetricas.TEMPERATURA) {
+            return <FontAwesome name="thermometer" color={"#FFAC7D"} size={25} />
+        }
+        if (item.categoria == EMetricas.HORAS_DORMIDAS) {
+            return <FontAwesome name="bed" color={"#4B0082"} size={25} />
+        }
+        if (item.categoria == EMetricas.ALTURA) {
+            return <Entypo name="ruler" color={"#000"} size={25} style={{ opacity: 0.8 }} />
+        }
+        if (item.categoria == EMetricas.IMC) {
+            return <Entypo name="calculator" color={"#000"} size={25} />
         }
     }
 
+
+    const getMetricas = () => {
+        const filter: IMetricaValueFilter = { idMetrica: item.id }
+        getAllMetricaValues(filter, order)
+            .then((response) => {
+                const newMetricasVAlues = response.data as IValorMetrica[];
+                setValorMetrica(newMetricasVAlues[0]);
+            })
+            .catch((err) => {
+                const error = err as { message: string };
+                Toast.show({
+                    type: "error",
+                    text1: "Erro!",
+                    text2: error.message,
+                });
+            })
+    };
+
+    const separaDataHora = () => {
+        setDataHora(valorMetrica?.dataHora as string)
+
+        if (!dataHora) return
+
+        const value = dataHora;
+        const valueFinal = value.split("T");
+        const separaHora = valueFinal[1].split(":");
+        setHora(`${separaHora[0]}:${separaHora[1]}`);
+    };
+
+    useEffect(getMetricas, []);
+    useEffect(() => separaDataHora(), [dataHora, valorMetrica]);
+
     return (
-      <ScrollView contentContainerStyle={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
             <View style={[styles.card, { borderColor: '#ddd', backgroundColor: '#fff' }]}>
-                <View style={ item.categoria == EMetricas.SATURACAO_OXIGENIO ? styles.oxygenIcon : styles.othersIcons}>
+                <View style={item.categoria == EMetricas.SATURACAO_OXIGENIO ? styles.oxygenIcon : styles.othersIcons}>
                     {icone()}
                     <Text style={[styles.title, { color: titleColor }]}>{item.categoria}</Text>
                 </View>
                 <Text style={styles.content}>
-                    {/* <Text style={[styles.number]}>{item.valor}</Text> */}
+                    {valorMetrica && (
+                        <Text style={[styles.number]}>{valorMetrica.valor}</Text>
+                    )}
                     <Text style={[styles.units, { color: textColor }]}>{unidade()}</Text>
                 </Text>
-                <Text style={[styles.time, { color: textColor }]}>{"16:00"}</Text>
+                <Text style={[styles.time, { color: textColor }]}>{hora}</Text>
                 <Icon name="chevron-right" size={16} color={textColor} style={styles.chevron} />
             </View>
         </ScrollView>
@@ -79,10 +139,10 @@ export default function CardMetrica({ item }: IProps) {
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     texto: {
         alignSelf: "center",
@@ -93,19 +153,19 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         padding: 16,
         margin: 8,
-        width: '100%', 
-        height:150,
+        width: '100%',
+        height: 150,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.4,
         shadowRadius: 4,
-        elevation: 3, 
+        elevation: 3,
     },
     title: {
         fontWeight: 'bold',
         fontSize: 14,
         marginBottom: 8,
-        marginLeft:8
+        marginLeft: 8
     },
     content: {
         fontSize: 14,
@@ -113,10 +173,10 @@ const styles = StyleSheet.create({
     },
     number: {
         fontWeight: 'bold',
-        fontSize: 24, 
+        fontSize: 24,
     },
     units: {
-        fontSize: 18, 
+        fontSize: 18,
     },
     time: {
         color: '#888',
@@ -124,17 +184,17 @@ const styles = StyleSheet.create({
         marginTop: 8,
     },
     chevron: {
-      top: 10,
-      left: '85%', 
-      position: 'absolute', 
-  },
-  othersIcons: {
-      flexDirection: "row",
-      alignItems: "center"
-  },
-  oxygenIcon: {
-      flexDirection: "row",
-      alignItems: "baseline"
-  },
+        top: 10,
+        left: '85%',
+        position: 'absolute',
+    },
+    othersIcons: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    oxygenIcon: {
+        flexDirection: "row",
+        alignItems: "baseline"
+    },
 
 });
