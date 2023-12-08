60a5a0cab31d0eff73debc8d83665ee8c951d21d
gabryelns <gabryelnick91@gmail.com>
2023-12-08 01:59:21 -0300

    (#116) adiciona teste cardValorMetrica

---
+++ src/app/__tests__/cardValorMetrica.spec.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import CardValorMetrica from '../components/CardValorMetrica';
+import { EMetricas } from '../interfaces/metricas.interface';
+import { ETipoSanguineo } from '../interfaces/idoso.interface';
+
+const mockItem = {
+    categoria: EMetricas.FREQ_CARDIACA,
+    id: 1,
+    valor: '80',
+    dataHora: '2023-01-01T12:30:00',
+    idMetrica: 123,  };
+
+const mockItem1 = {
+    categoria: EMetricas.GLICEMIA,
+    id: 1,
+    valor: '80',
+    dataHora: '2023-01-01T12:30:00',
+    idMetrica: 123,  };
+
+const mockItem2 = {
+    categoria: EMetricas.PESO,
+    id: 1,
+    valor: '80',
+    dataHora: '2023-01-01T12:30:00',
+    idMetrica: 123,  };
+
+const mockItem3 = {
+    categoria: EMetricas.PRESSAO_SANGUINEA,
+    id: 1,
+    valor: '80',
+    dataHora: '2023-01-01T12:30:00',
+    idMetrica: 123,  };
+
+const mockItem4 = {
+    categoria: EMetricas.SATURACAO_OXIGENIO,
+    id: 1,
+    valor: '80',
+    dataHora: '2023-01-01T12:30:00',
+    idMetrica: 123,  };
+
+const mockItem5 = {
+    categoria: EMetricas.TEMPERATURA,
+    id: 1,
+    valor: '80',
+    dataHora: '2023-01-01T12:30:00',
+    idMetrica: 123,  };
+
+
+describe('CardValorMetrica Component', () => {
+
+  it('renderiza corretamente', () => {
+    const { getByText } = render(<CardValorMetrica item={mockItem} />);
+
+    expect(getByText('80')).toBeDefined();
+    expect(getByText('bpm')).toBeDefined();
+    expect(getByText('01/01/2023')).toBeDefined();
+    expect(getByText('12:30')).toBeDefined();
+  });
+
+  it('exibe as unidades corretas 1', () => {
+    const { getByText } = render(<CardValorMetrica item={mockItem1} />);
+    
+    if (mockItem1.categoria === EMetricas.GLICEMIA) {
+      expect(getByText('mg/dL')).toBeTruthy();
+    } else {
+      expect(() => getByText('mg/dL')).toThrow();
+    }
+  });
+
+  it('exibe as unidades corretas 2', () => {
+    const { getByText } = render(<CardValorMetrica item={mockItem2} />);
+    
+    if (mockItem2.categoria === EMetricas.PESO) {
+      expect(getByText('kg')).toBeTruthy();
+    } else {
+      expect(() => getByText('kg')).toThrow();
+    }
+  });
+  it('exibe as unidades corretas 3', () => {
+    const { getByText } = render(<CardValorMetrica item={mockItem3} />);
+    
+    if (mockItem3.categoria === EMetricas.PRESSAO_SANGUINEA) {
+      expect(getByText('mmHg')).toBeTruthy();
+    } else {
+      expect(() => getByText('mmHg')).toThrow();
+    }
+  });
+  it('exibe as unidades corretas 4', () => {
+    const { getByText } = render(<CardValorMetrica item={mockItem4} />);
+    
+    if (mockItem4.categoria === EMetricas.SATURACAO_OXIGENIO) {
+      expect(getByText('%')).toBeTruthy();
+    } else {
+      expect(() => getByText('%')).toThrow();
+    }
+  });
+
+  it('exibe as unidades corretas 5', () => {
+    const { getByText } = render(<CardValorMetrica item={mockItem5} />);
+    
+    if (mockItem5.categoria === EMetricas.TEMPERATURA) {
+      expect(getByText('°C')).toBeTruthy();
+    } else {
+      expect(() => getByText('°C')).toThrow();
+    }
+  });
+
+  
+
+});
+
