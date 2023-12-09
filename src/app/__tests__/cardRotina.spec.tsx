9b5c3b8b9fdf2178d20b33c137fa7f628f054725
Gustavo <gugustavo137@gmail.com>
2023-12-08 21:21:06 -0300

    (#116) adiciona teste para card de medicamento e exercicio
    Co-authored-by: melohugo <hugoq2004@gmail.com>
    Co-authored-by: Guilherme Silva <221021984@aluno.unb.br>

--- src/app/__tests__/cardRotina.spec.tsx
+++ src/app/__tests__/cardRotina.spec.tsx
@@ -12,7 +12,7 @@ import { ECategoriaRotina, EDiasSemana } from '../interfaces/rotina.interface';
   }));
 
   const rotina = {
-    id: 1,
+  id: 1,
   titulo: 'Título de Exemplo',
   idIdoso: 123, 
   categoria: ECategoriaRotina.ALIMENTACAO, 
@@ -22,13 +22,46 @@ import { ECategoriaRotina, EDiasSemana } from '../interfaces/rotina.interface';
   dias: [EDiasSemana.Domingo],
   };
 
-describe('card Rotina Component', () => {
+  const rotina_exercicios = {
+  id: 2,
+  titulo: 'Card exercicio',
+  idIdoso: 456, 
+  categoria: ECategoriaRotina.EXERCICIOS, 
+  descricao: 'caminhada',
+  dataHoraConcluidos: [],
+  dataHora: new Date(),
+  dias: [EDiasSemana.Domingo],
+  };
+
+  const rotina_medicamentos = {
+  id: 3,
+  titulo: 'Card medicamento',
+  idIdoso: 789, 
+  categoria: ECategoriaRotina.MEDICAMENTO, 
+  descricao: 'dipirona',
+  dataHoraConcluidos: [],
+  dataHora: new Date(),
+  dias: [EDiasSemana.Domingo],
+  };
+
+describe('Teste Componente Card Rotina', () => {
     act(() => {
-        test('renders routine information correctly', () => {
+        test('Renderiza corretamente', () => {
             const { getByText } = render(<CardRotina item={rotina} index={0} date={new Date()} />);
             expect(getByText('Título de Exemplo')).toBeTruthy();
             expect(getByText('Descrição de Exemplo')).toBeTruthy();
-          });
+        });
+        test('Renderiza corretamente Card de exercicios', () => {
+          const { getByText } = render(<CardRotina item={rotina_exercicios} index={0} date={new Date()} />);
+          expect(getByText('Card exercicio')).toBeTruthy();
+          expect(getByText('caminhada')).toBeTruthy();
+        });
+        test('Renderiza corretamente Card de Medicamentos', () => {
+          const { getByText } = render(<CardRotina item={rotina_medicamentos} index={0} date={new Date()} />);
+          expect(getByText('Card medicamento')).toBeTruthy();
+          expect(getByText('dipirona')).toBeTruthy();
+        });
+
     });
 
 });
